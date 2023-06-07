f1c6bdd0f20ea5e67948c352187bc91a1f9fc65b
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-07 15:05:42 -0400

    prompt for input and adjust volume calculation

--- psets/2-c-fundamentals/sphere.c
+++ psets/2-c-fundamentals/sphere.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-// calculates the volume of a sphere with a 10 meter radius 
+// calculates the volume of a sphere 
 int main(void)
 {
 	// v = 4/3πr³
-	float r = 10.0;
-	float pie = 3.14159;
-	float v = (4.0f / 3.0f) * (pie * (r * r * r));
-	printf("The volume of a sphere with a radius of 10 meters is %.2f cubic meters\n", v);
+	printf("Enter the radius in meters: ");
+	float r;
+	scanf("%f", &r);
+	float pi = 3.14159;
+	float v = (4.0f / 3.0f) * pi * (r * r * r);
+	printf("The volume of a sphere with a radius of %.2f meters is %.2f cubic meters\n", r, v);
 	return 0;
 }
