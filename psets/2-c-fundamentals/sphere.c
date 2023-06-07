2f685f4df7d4eb942ffafa1000c908b92554b885
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-07 14:45:20 -0400

    calculate volume

--- psets/2-c-fundamentals/sphere.c
+++ psets/2-c-fundamentals/sphere.c
@@ -1,1 +1,12 @@
-/* calculates the volume of a sphere with a 10 meter radius */
+#include <stdio.h>
+
+// calculates the volume of a sphere with a 10 meter radius 
+int main(void)
+{
+	// v = 4/3πr³
+	float r = 10.0;
+	float pie = 3.14159;
+	float v = (4.0f / 3.0f) * (pie * (r * r * r));
+	printf("The volume of a sphere with a radius of 10 meters is %.2f cubic meters\n", v);
+	return 0;
+}
