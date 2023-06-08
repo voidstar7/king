5bc568370a0a6af9a2c8468b80026d75b0827c64
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-08 08:15:23 -0400

    declare amount and tax rate on same line

--- psets/2-c-fundamentals/tax.c
+++ psets/2-c-fundamentals/tax.c
@@ -4,10 +4,10 @@
 
 int main(void)
 {
-	float amount;
+	float amount, taxRate;
+	taxRate = 0.05;
+
 	printf("Enter an amount: ");
 	scanf("%f", &amount);
-
-	float taxRate = 0.05;
 	printf("With tax added: %.2f\n", amount * (1 + taxRate));
 }
