a03f3b9a6599910f8a3f350597a61c97c4df1ff5
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-18 21:20:59 -0400

    use single in as input, reverse using %

--- psets/4-expressions/reverse.c
+++ psets/4-expressions/reverse.c
@@ -3,9 +3,15 @@
 // prompts for a two digit number, then prints the number reversed
 int main(void)
 {
-	int first, second;
-	printf("Enter a two digit number ");
-	scanf("%1d%1d", &first, &second);
+	int number, first, second;
+
+	printf("Enter a two digit number: ");
+	scanf("%2d", &number);
+
+	second = number % 10;
+	first = (number - second) / 10;
+
 	printf("The reversal is: %d%d\n", second, first);
+
 	return 0;
 }
