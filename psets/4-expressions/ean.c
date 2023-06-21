881e80a3ad0da7e6a26f879c549a7e317a56044a
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-21 16:06:36 -0400

    copy upc.c code to ean.c

---
+++ psets/4-expressions/ean.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// computes the check digit of a 13 digit EAN barcode
+
+int main(void)
+{
+	int type, manufacturer, product; 
+	printf("Enter the first 11 digits of the UPC: ");
+	scanf("%1d%5d%5d", &type, &manufacturer, &product);
+
+	int odd = 0; odd += type;
+	int even = 0;
+
+	even += (manufacturer % 10); manufacturer /= 10;
+	odd += (manufacturer % 10); manufacturer /= 10;
+	even += (manufacturer % 10); manufacturer /= 10;
+	odd += (manufacturer % 10); manufacturer /= 10;
+	even += (manufacturer % 10); manufacturer /= 10;
+	even += (product % 10); product /= 10;
+	odd += (product % 10); product /= 10;
+	even += (product % 10); product /= 10;
+	odd += (product % 10); product /= 10;
+	even += (product % 10); product /= 10;
+	
+	printf("The check digit is %d\n", 9 - ((((odd * 3) + even) - 1) % 10));
+	return 0;
+}
