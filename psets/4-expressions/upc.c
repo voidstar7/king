9d73918be2f866b312e54c2f6cc51970e707da4a
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-20 23:27:00 -0400

    compute check digit from single input

--- psets/4-expressions/upc.c
+++ psets/4-expressions/upc.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-// computes the checksum of a upc barcode
+// computes the check digit of a upc barcode
 
 int main(void)
 {
-	int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, first_sum, second_sum, total;
-	printf ("Enter the first (single) digit: ");
-	scanf("%1d", &d);
-	printf ("Enter first group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3 ,&i4 , &i5);
-	printf ("Enter second group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+	int type, manufacturer, product; 
+	printf("Enter the first 11 digits of the UPC: ");
+	scanf("%1d%5d%5d", &type, &manufacturer, &product);
 
-	first_sum = d + i2 + i4 + j1 + j3 + j5; 
-	second_sum= i1 + i3 + i5 + j2+ j4; 
-	total = 3 * first_sum + second_sum;
-	printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+	int odd = 0; odd += type;
+	int even = 0;
+
+	even += (manufacturer % 10); manufacturer /= 10;
+	odd += (manufacturer % 10); manufacturer /= 10;
+	even += (manufacturer % 10); manufacturer /= 10;
+	odd += (manufacturer % 10); manufacturer /= 10;
+	even += (manufacturer % 10); manufacturer /= 10;
+	even += (product % 10); product /= 10;
+	odd += (product % 10); product /= 10;
+	even += (product % 10); product /= 10;
+	odd += (product % 10); product /= 10;
+	even += (product % 10); product /= 10;
+	
+	printf("The check digit is %d\n", 9 - ((((odd * 3) + even) - 1) % 10));
 	return 0;
 }
