e487a426a42a14e0d94cfb2b086c713523a82d64
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-20 22:22:33 -0400

    delete test print statement

--- psets/4-expressions/upc.c
+++ psets/4-expressions/upc.c
@@ -15,7 +15,6 @@ int main(void)
 	first_sum = d + i2 + i4 + j1 + j3 + j5; 
 	second_sum= i1 + i3 + i5 + j2+ j4; 
 	total = 3 * first_sum + second_sum;
-	//printf("Check digit: %d\n", 9 - ((total - 1) % 10));
-	printf("Check digit: %d\n", (10 - (total - 10)) % 10);
+	printf("Check digit: %d\n", 9 - ((total - 1) % 10));
 	return 0;
 }
