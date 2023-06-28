ed09ae6e656e808fdb482ae2559de18e2a910a0a
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-28 07:09:04 -0400

    optimize if conditions

--- psets/5-selection-statements/beaufort.c
+++ psets/5-selection-statements/beaufort.c
@@ -6,13 +6,13 @@ int main(void) {
 	printf("Enter the wind speed in knots: "); scanf("%d", &speed);
 	if (speed < 1)
 		printf("Calm\n");
-	else if (speed >= 1 && speed <= 3)
+	else if (speed <= 3)
 		printf("Light air\n");
-	else if (speed >= 4 && speed <= 27)
+	else if (speed <= 27)
 		printf("Breeze\n");
-	else if (speed >= 28 && speed <= 47)
+	else if (speed <= 47)
 		printf("Gale\n");
-	else if (speed >= 48 && speed <= 63)
+	else if (speed <= 63)
 		printf("Storm\n");
 	else 
 		printf("Hurricane\n");
