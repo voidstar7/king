175b40e35914a8fa820e8aeb74e2edec0f647513
ex07 <119447705+ex07@users.noreply.github.com>
2023-06-29 22:51:01 -0400

    fix bug in odd/even expressions; add checksum validation

--- psets/4-expressions/upc.c
+++ psets/4-expressions/upc.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
 // computes the check digit of a 12 digit UPC barcode
-
+// example: 0 13800 15173 5
 int main(void)
 {
-	int type, manufacturer, product; 
-	printf("Enter the first 11 digits of the UPC: ");
-	scanf("%1d%5d%5d", &type, &manufacturer, &product);
-
-	int odd = 0; odd += type;
+	int type, manufacturer, product, check, checksum; 
+	int odd = 0; 
 	int even = 0;
+	printf("Enter the UPC: ");
+	scanf("%1d %5d %5d %1d", &type, &manufacturer, &product, &check);
+		odd += type;
+		even += (manufacturer % 10); manufacturer /= 10;
+		odd += (manufacturer % 10); manufacturer /= 10;
+		even += (manufacturer % 10); manufacturer /= 10;
+		odd += (manufacturer % 10); manufacturer /= 10;
+		even += (manufacturer % 10); manufacturer /= 10;
+		odd += (product % 10); product /= 10;
+		even += (product % 10); product /= 10;
+		odd += (product % 10); product /= 10;
+		even += (product % 10); product /= 10;
+		odd += (product % 10); product /= 10;
 
-	even += (manufacturer % 10); manufacturer /= 10;
-	odd += (manufacturer % 10); manufacturer /= 10;
-	even += (manufacturer % 10); manufacturer /= 10;
-	odd += (manufacturer % 10); manufacturer /= 10;
-	even += (manufacturer % 10); manufacturer /= 10;
-	even += (product % 10); product /= 10;
-	odd += (product % 10); product /= 10;
-	even += (product % 10); product /= 10;
-	odd += (product % 10); product /= 10;
-	even += (product % 10); product /= 10;
-	
-	printf("The check digit is %d\n", 9 - ((((odd * 3) + even) - 1) % 10));
-	return 0;
+	checksum = 9 - ((((odd * 3) + even) - 1) % 10);
+	if (check == checksum) {
+		printf("VALID\n");
+		return 0;
+	}	
+	printf("INVALID\n");
+	return 1;
 }
