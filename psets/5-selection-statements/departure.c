e87aa5e166cefd589c4ab7ca89ffb0c281ccb811
ex07 <119447705+ex07@users.noreply.github.com>
2023-07-02 22:29:10 -0400

    complete departure.c

--- psets/5-selection-statements/departure.c
+++ psets/5-selection-statements/departure.c
@@ -3,116 +3,75 @@
 // prompts for a time in 24 hour format, then displays (in standard format) the departure and arrival times of the flight whose departure time is closest to the user input
 
 int main(void) {
-	int hour, minutes, conversion, 
-			da, db, dc, dd, de, df, dg, dh,
-			ca, cb, cc, cd, ce, cf, cg, ch,
-			hda, hdb, hdc, hdd, hde, hdf, hdg, hdh,
-			mda, mdb, mdc, mdd, mde, mdf, mdg, mdh,
-			haa, hab, hac, had, hae, haf, hag, hah,
-			maa, mab, mac, mad, mae, maf, mag, mah,
-	departHour, departMinutes,
-	arriveHour, arriveMinutes;
-
+	int a, b, c, d, e, f, g, h, 
+			hour, minutes, time;
 	printf("Enter a time (24 hour format xx:xx): ");
 	scanf("%d:%d", &hour, &minutes);
-	conversion = hour * 60 + minutes;
+	time = hour * 60 + minutes;
 
-	// depart = 8:00am; arrive = 10:16am
-		 da = 8 * 60 + 0; 
-		 ca = conversion - da;
-		 hda = 8; mda = 0;
-		 haa = 10; maa = 16; 
+	// a | depart = 8:00am; arrive = 10:16am
+	a = (8 * 60 + 0);
+	// b | depart = 9:43am; arrive = 11:52am
+	b = (9 * 60 + 43);
+	// c | depart = 11:19a; arrive = 1:31pm
+	c = (11 * 60 + 19);
+	// d | depart = 12:47pm; arrive = 3:00pm
+	d = (12 * 60 + 47);
+	// e | depart = 2:00pm; arrive = 4:08pm
+	e = (14 * 60 + 0);
+	// f | depart = 3:45pm; arrive = 5:55pm
+	f = (15 * 60 + 45);
+	// g | depart = 7:00pm; arrive = 9:20pm
+	g = (19 * 60 + 0);
+	// h | depart = 9:45pm; arrive = 11:58pm
+	h = (21 * 60 + 45);
 
-	// depart = 9:43am; arrive = 11:52am
-		 db = 9 * 60 + 43;
-		 cb = conversion - db;
-		 hdb = 9; mdb = 43;
-		 hab = 11; mab = 52;
-		 
-	// depart = 11:19a; arrive = 1:31pm
-		 dc = 11 * 60 + 19;
-		 cc = conversion - dc;
-		 hdc = 11; mdc = 19;
-		 hac = 1; mac = 31;
-
-	// depart = 12:47pm; arrive = 3:00pm
-		 dd = 12 * 60 + 47;
-		 cd = conversion - dd;
-		 hdd = 12; mdd = 47;
-		 had = 3; mad = 0;
-
-	// depart = 2:00pm; arrive = 4:08pm
-		 de = 14 * 60 + 0;
-		 ce = conversion - de;
-		 hde = 2; mde = 0;
-		 hae = 4; mae = 8;
-
-	// depart = 3:45pm; arrive = 5:55pm
-		 df = 15 * 60 + 45;
-		 cf = conversion - df;
-		 hdf = 3; mdf = 45;
-		 haf = 5; maf = 55;
-
-	// depart = 7:00pm; arrive = 9:20pm
-		 dg = 19 * 60 + 0;
-		 cg = conversion - dg;
-		 hdg = 7; mdg = 0;
-		 hag = 9; mag = 20;
-		 
-	// depart = 9:45pm; arrive = 11:58pm
-		 dh = 21 * 60 + 45;
-		 ch = conversion - dh;
-		 hdh = 9; mdh = 45;
-		 hah = 11; mah = 58;
-
-	if (ca < cb && ca < cc && ca < cd && ca < ce && ca < cf && ca < cg && ca < ch) {
-		departHour = hda; departMinutes = mda;
-		arriveHour = haa; arriveMinutes = maa;
+	if (time < a) {
+		printf("Closest departure time is 8:00am, arriving at 10:16am\n");
 	}
-	else if (cb < ca && cb < cc && cb < cd && cb < ce && cb < cf && cb < cg && cb < ch) {
-		departHour = hdb; departMinutes = mdb;
-		arriveHour = hab; arriveMinutes = mab;
+	else if (time >= a && time < b) {
+		if ((time - a) < (b - time)) 
+			printf("Closest departure time is 8:00am, arriving at 10:16am\n");
+		else
+			printf("Closest departure time is 9:43am, arriving at 11:52am\n");
 	}
-	else if (cc < cb && cc < ca && cc < cd && cc < ce && cc < cf && cc < cg && cc < ch) {
-		departHour = hdc; departMinutes = mdc;
-		arriveHour = hac; arriveMinutes = mac;
+	else if (time >= b && time < c) {
+		if ((time - b) < (c - time)) 
+			printf("Closest departure time is 9:43am, arriving at 11:52am\n");
+		else
+			printf("Closest departure time is 11:19am, arriving at 1:31pm\n");
 	}
-	else if (cd < cb && cd < cc && cd < ca && cd < ce && cd < cf && cd < cg && cd < ch) {
-		departHour = hdd; departMinutes = mdd;
-		arriveHour = had; arriveMinutes = mad;
+	else if (time >= c && time < d) {
+		if ((time - c) < (d - time)) 
+			printf("Closest departure time is 11:19am, arriving at 1:31pm\n");
+		else
+			printf("Closest departure time is 12:47pm, arriving at 3:00pm\n");
 	}
-	else if (ce < cb && ce < cc && ce < cd && ce < ca && ce < cf && ce < cg && ce < ch) {
-		departHour = hde; departMinutes = mde;
-		arriveHour = hae; arriveMinutes = mae;
+	else if (time >= d && time < e) {
+		if ((time - d) < (e - time)) 
+			printf("Closest departure time is 12:47pm, arriving at 3:00pm\n");
+		else
+			printf("Closest departure time is 2:00pm, arriving at 4:08pm\n");
 	}
-	else if (cf < cb && cf < cc && cf < cd && cf < ce && cf < ca && cf < cg && cf < ch) {
-		departHour = hdf; departMinutes = mdf;
-		arriveHour = haf; arriveMinutes = maf;
+	else if (time >= e && time < f) {
+		if ((time - e) < (f - time)) 
+			printf("Closest departure time is 2:00pm, arriving at 4:08pm\n");
+		else
+			printf("Closest departure time is 3:45pm, arriving at 5:55pm\n");
 	}
-	else if (cg < cb && cg < cc && cg < cd && cg < ce && cg < cf && cg < ca && cg < ch) {
-		departHour = hdg; departMinutes = mdg;
-		arriveHour = hag; arriveMinutes = mag;
+	else if (time >= f && time < g) {
+		if ((time - f) < (g - time)) 
+			printf("Closest departure time is 3:45pm, arriving at 5:55pm\n");
+		else
+			printf("Closest departure time is 7:00pm, arriving at 9:20pm\n");
 	}
-	else if (ch	< cb && ch < cc && ch < cd && ch < ce && ch < cf && ch < cg && ch < ca) {
-		departHour = hdh; departMinutes = mdh;
-		arriveHour = hah; arriveMinutes = mah;
+	else if (time >= g && time <= h) {
+		if ((time - g) < (h - time)) 
+			printf("Closest departure time is 7:00pm, arriving at 9:20pm\n");
+		else
+			printf("Closest departure time is 9:45pm, arriving at 11:58pm\n");
 	}
 	else {
-		printf("Error\n");
-		return 1;
-	} 
-
-	if (departHour < (12 * 60) && arriveHour < (12 * 60)) {
-		printf("Closest departure time is %d:%.2dam, arriving at %d:%2dam\n", 
-				departHour, departMinutes, arriveHour, arriveMinutes);
+		printf("There are no more departures scheduled for today\n");
 	}
-	else if (departHour > (12 * 60) && arriveHour > (12 * 60)) {
-		printf("Closest departure time is %d:%.2dpm, arriving at %d:%2dpm\n", 
-				departHour, departMinutes, arriveHour, arriveMinutes);
-	}
-	else {
-		printf("Closest departure time is %d:%.2dam, arriving at %d:%2dpm\n", 
-				departHour, departMinutes, arriveHour, arriveMinutes);
-	}
-	return 0;
 }
