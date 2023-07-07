5f3838b6a3d1dd2ec27225a79b1ef2964b532a94
ex07 <119447705+ex07@users.noreply.github.com>
2023-07-06 21:41:48 -0400

    add continue and goto statements

--- psets/6-loops/addition-calculator.c
+++ psets/6-loops/addition-calculator.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-	int n, sum = 0;
-	printf("Enter numbers to compute their sum (0 to exit): ");
-	do {
-		scanf("%d", &n);
-		sum += n;
+	int i, n = 0, sum = 0;
+	printf("Enter up to 10 numbers except 4 to compute their sum (press 0 to exit): ");
+	while (n < 10) {
+		scanf("%d", &i);
+		if (i == 4) {
+			printf("Invalid number!\n");
+			continue;
+		}
+		else if (i == 0) {
+			printf("Exiting\n");
+			goto exit;
+		}
+		sum += i;
+		n++;
 	}
-	while (n != 0);
+	exit:
 	printf("The sum is %d\n", sum);
 }
