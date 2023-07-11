cef9e4c4caea66d47a32cd6c69fe777a1cbf4727
ex07 <119447705+ex07@users.noreply.github.com>
2023-07-10 21:37:46 -0400

    allow number of any length

--- psets/4-expressions/reverse.c
+++ psets/4-expressions/reverse.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-// prompts for a three digit number, then prints the number reversed
-int main(void)
-{
-	int number, first, second, third;
-
-	printf("Enter a three digit number: ");
-	scanf("%3d", &number);
-
-	third = number % 10;
-	number = (number - third) / 10;
-	second = number % 10;
-	first = (number - second) / 10;
-
-	printf("The reversal is: %d%d%d\n", third, second, first);
-
+// prompts for a digit number, then prints the number reversed
+int main(void) {
+	int n, r;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	do {
+		printf("%d", n % 10);		
+		n /= 10;
+	}
+	while (n != 0);
+	printf("\n");
 	return 0;
-
-	/* can be simplified to only use one variable and three lines of code
-	 
-  printf("Enter a three-digit number: ");
-  scanf("%d", &n);
-  printf("The reversal is: %d%d%d\n", n % 10, (n / 10) % 10, n / 100);
-
-	*/
 }
