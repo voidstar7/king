2d54456afc41e0ed40622c523f13bd3632920b50
ex07 <119447705+ex07@users.noreply.github.com>
2023-07-13 22:31:20 -0400

    allow user to specify number of payments

--- psets/2-c-fundamentals/loan.c
+++ psets/2-c-fundamentals/loan.c
@@ -4,24 +4,26 @@
 
 int main(void)
 {
-	float principle, interestRate, monthlyInterestRate, monthlyPayment, first, second, third;
+	float principle, interestRate, monthlyPayment, balance;
+	int numberPayments;
 
-	printf("Enter amount of loan: ");
-	scanf("%f", &principle);
-	printf("Enter the interest rate: ");
-	scanf("%f", &interestRate);
-	printf("Enter the monthly payment: ");
-	scanf("%f", &monthlyPayment);
+	printf("Enter amount of loan: "); scanf("%f", &principle);
+	printf("Enter the interest rate: "); scanf("%f", &interestRate);
+	printf("Enter the monthly payment: "); scanf("%f", &monthlyPayment);
+	printf("Enter the number of payments: "); scanf("%d", &numberPayments);
 
-	monthlyInterestRate = (interestRate / 100) / 12;
-
-	first = principle * (1.0f + monthlyInterestRate) - monthlyPayment;
-	second = first * (1.0f + monthlyInterestRate) - monthlyPayment;
-	third = second * (1.0f + monthlyInterestRate) - monthlyPayment;
-
-	printf("Balance remaining after first payment: %.2f\n", first);
-	printf("Balance remaining after second payment: %.2f\n", second);
-	printf("Balance remaining after third payment: %.2f\n", third );
-
+	if (numberPayments <= 0) {
+		printf("Balance: %.2f\n", principle);
+		return 0;
+	}
+	float monthlyInterestRate = (interestRate / 100) / 12;
+	if (numberPayments == 1)
+		return 0;
+	for (int i = 1; i <= numberPayments; i++) {
+		principle *= 1.0f + monthlyInterestRate;
+		balance = principle - monthlyPayment;
+		printf("Balance remaining after payment %d: %.2f\n", i, balance);
+		principle -= monthlyPayment;
+	}
 	return 0;
 }
