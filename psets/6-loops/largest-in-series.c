b76eb16ebffafc05cb47d39fa99d9d2f244be2c1
ex07 <119447705+ex07@users.noreply.github.com>
2023-07-14 21:48:30 -0400

    complete largest-in-series.c

--- psets/6-loops/largest-in-series.c
+++ psets/6-loops/largest-in-series.c
@@ -3,19 +3,17 @@
 // prompts the user to enter numbers one by one, then prints the largest of the series when 0 or a negative number are entered
 
 int main(void) {
-	int n, largest = 0;
+	float n, largest = 0;
 	do {
 		printf("Enter a number: "); 
-		scanf("%d", &n);
+		scanf("%f", &n);
 		if (n > 0) {
 			if (n > largest)
 				largest = n;
 			continue;
 		}
-		else
-			break;
+		break;
 	}
 	while (n > 0);
-	printf("The largest number entered was %d\n", largest);
-	return 0;
+	printf("The largest number entered was %g\n", largest);
 }
