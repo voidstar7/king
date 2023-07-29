50eedbe25cbcb304a838421ee005b310759d8389
ex07 <119447705+ex07@users.noreply.github.com>
2023-07-28 21:53:40 -0400

    add cases results are not simplified

--- psets/3-formatted-input-output/math-fraction.c
+++ psets/3-formatted-input-output/math-fraction.c
@@ -4,11 +4,40 @@
 
 int main (void)
 {
-	int n1, d1, n2, d2;
+	int n1, d1, n2, d2, n3, d3;
 	char op;
-
 	printf("Enter two fractions separated by an operator: ");
 	scanf("%d / %d %c %d / %d", &n1, &d1, &op, &n2, &d2);
-
+	switch(op)
+	{
+		case '+':
+			if (d1 == d2)
+			{
+				n3 = n1 + n2;
+				d3 = d1;
+				break;
+			}
+			n3 = (n1 * d2) + (n2 * d1);
+			d3 = d1 * d2;
+			break;
+		case '-':
+			if (d1 == d2)
+			{
+				n3 = n1 - n2;
+				d3 = d1;
+				break;
+			}
+			n3 = (n1 * d2) - (n2 * d1);
+			d3 = d1 * d2;
+			break;
+		case '*':
+			n3 = n1 * n2;
+			d3 = d1 * d2;
+		case '/':
+			n3 = n1 * d2;
+			d3 = d1 * n2;
+			break;
+	}
+	printf("%d/%d", n3, d3);
 	return 0;
 }
