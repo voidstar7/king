e9dcbd496e08dbe80d4496e6e4d9080ebbc549da
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-06 15:06:25 -0400

    add flags and switch statement

--- psets/7-basic-types/float-evaluation.c
+++ psets/7-basic-types/float-evaluation.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-// evaluates an expression of floats from left to right regardless of operator precedence. Possible operators are +, -, *, and
+// evaluates an expression of from left to right regardless of operator precedence. Possible operators are +, -, *, and /
 
 int main(void)
 {
-	char c;
-	float value = 0;
-
+	bool number, decimal, add, subtract, multiply, divide;
+	int scale = 0;
+	float value = 0.0f;
 	printf("Enter an expression: ");
-	c = getchar();
-	c -= 48;
-	value += (float)c;
-	printf("value: %f", value);
+
+input:
+	while (1)
+	{
+		char c = getchar();
+		if (isdigit(c))
+		{
+			number = true;
+			scale++;
+			break;
+		}
+		switch (c)
+		{
+			case '10': 
+				goto end;
+			case '32': 
+				continue;
+			case '43':
+				add = true;	
+				break;
+			case '45':
+				subtract = true; 
+				break;
+			case '42':
+				multiply = true; 
+				break;
+			case '47':
+				divide = true;
+				break;
+			case '46':
+				decimal = true; 
+				break;
+			default:
+				continue;
+		}
+
+end:
+		printf("Value of expression: ");
+		return 0;
+	}
 }
