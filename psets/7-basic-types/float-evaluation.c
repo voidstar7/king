05c025e4c1a132d6c2965aa91376a763683049b1
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-06 15:41:41 -0400

    read user number input and convert to float digit

--- psets/7-basic-types/float-evaluation.c
+++ psets/7-basic-types/float-evaluation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdbool.h>
 
 // evaluates an expression of from left to right regardless of operator precedence. Possible operators are +, -, *, and /
@@ -7,13 +8,15 @@ int main(void)
 {
 	bool number, decimal, add, subtract, multiply, divide;
 	int scale = 0;
+	float digit = 0.0f;
 	float value = 0.0f;
+	char c;
 	printf("Enter an expression: ");
 
 input:
 	while (1)
 	{
-		char c = getchar();
+		c = getchar();
 		if (isdigit(c))
 		{
 			number = true;
@@ -22,31 +25,47 @@ input:
 		}
 		switch (c)
 		{
-			case '10': 
+			case 10: 
 				goto end;
-			case '32': 
+			case 32: 
+				scale = 0;
 				continue;
-			case '43':
+			case 43:
 				add = true;	
+				scale = 0;
 				break;
-			case '45':
+			case 45:
 				subtract = true; 
+				scale = 0;
 				break;
-			case '42':
+			case 42:
 				multiply = true; 
+				scale = 0;
 				break;
-			case '47':
+			case 47:
 				divide = true;
+				scale = 0;
 				break;
-			case '46':
+			case 46:
 				decimal = true; 
+				scale++;
 				break;
 			default:
 				continue;
 		}
+	}
+	if (number == true)
+	{
+		if (scale == 1)
+		{
+			digit = (float)c - 48;
+			goto input;
+		}
+		digit = (digit * 10) + ((float)c - 48);
+		goto input;
+	}
 
 end:
-		printf("Value of expression: ");
+		printf("Value of expression: %f\n", digit);
 		return 0;
-	}
 }
