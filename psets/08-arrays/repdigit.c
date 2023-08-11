b44be5ccdef1d296a25b86cf68483b90d459169a
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-10 22:19:54 -0400

    allow multiple entries

--- psets/08-arrays/repdigit.c
+++ psets/08-arrays/repdigit.c
@@ -6,21 +6,28 @@ int main(void)
 	int digit;
 	long n;
 
-	printf("Enter a number: ");
+prompt:
+	printf("Enter a number (0 to terminate): ");
 	scanf("%ld", &n);
-	while (n > 0)
+	if (n > 0)
 	{
-		digit = n % 10;
-		digit_seen[digit]++;
-		n /= 10;
+		while (n > 0)
+		{
+			digit = n % 10;
+			digit_seen[digit]++;
+			n /= 10;
+		}
+		printf("Digit:      ");
+		for (int i = 0; i < 10; i++)
+			printf("%2d ", i);
+		printf("\n");
+		printf("Occurences: ");
+		for (int j = 0; j < 10; j++)
+			printf("%2d ", digit_seen[j]);
+		printf("\n");
+		for (int k = 0; k < 10; k++)
+			digit_seen[k] = 0;
+		goto prompt;
 	}
-	printf("Digit:      ");
-	for (int i = 0; i < 10; i++)
-		printf("%2d ", i);
-	printf("\n");
-	printf("Occurences: ");
-	for (int j = 0; j < 10; j++)
-		printf("%2d ", digit_seen[j]);
-	printf("\n");
 	return 0;
 }
