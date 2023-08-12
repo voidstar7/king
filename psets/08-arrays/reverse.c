b4e9a9fc470865a3f5eae34e48861b0f839a7c6a
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-11 22:11:18 -0400

    use sizeof() in for loops

--- psets/08-arrays/reverse.c
+++ psets/08-arrays/reverse.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#define	N 10
+#define N 10
 
 // reverses a series of numbers
 int main(void)
 {
-	int a[N], i;
+	int a[N];
+	int i; 
 	printf("Enter %d numbers: ", N);
-	for (i = 0; i < N; i++)
+	for (i = 0; i < (int)(sizeof(a) / sizeof(a[0])); i++)
 		scanf("%d", &a[i]);
 	printf("In reverse order:");
-	for (i = N - 1; i >= 0; i--)
+	for (i = ((int)(sizeof(a) / sizeof(a[0]))) - 1; i >= 0; i--)
 		printf(" %d", a[i]);
 	printf("\n");
 	return 0;
