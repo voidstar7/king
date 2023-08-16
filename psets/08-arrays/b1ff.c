390090f35520becceb567f435d52a7ef3595e483
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-15 21:43:16 -0400

    add switch statement

--- psets/08-arrays/b1ff.c
+++ psets/08-arrays/b1ff.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
+#include <ctype.h>
 #define SIZE 100
 
 // prompts for a message, converts letters to uppercase, substitutes some letters for digits, and appends ten exclamation marks
-// A 4
-// B 8
-// E 3
-// I 1
-// O 0
-// S 5
 int main(void)
 {
 	char message[SIZE] = {0};
@@ -19,7 +14,17 @@ int main(void)
 		c = getchar();
 		if (c == 10)
 			break;
-		message[i] = c;
+		c = toupper(c);
+		switch (c)
+		{
+			case 'A':	message[i] = '4'; break;
+			case 'B':	message[i] = '8'; break;
+			case 'E':	message[i] = '3'; break;
+			case 'I':	message[i] = '1'; break;
+			case 'O':	message[i] = '0'; break;
+			case 'S':	message[i] = '5'; break;
+			default: message[i] = c; break;
+		}
 		i++;
 	}
 	for (int j = 0; j < SIZE; j++)
@@ -28,6 +33,6 @@ int main(void)
 			break;
 		printf("%c", message[j]);
 	}
-	printf("\n");
+	printf("!!!!!!!!!!\n");
 	return 0;
 }
