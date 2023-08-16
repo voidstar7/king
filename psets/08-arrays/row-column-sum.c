35ac8615556883e5474662497bd5848eae93c756
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-16 17:20:15 -0400

    sum single digits in each row

---
+++ psets/08-arrays/row-column-sum.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#define R 5 
+#define C 5
+
+// prompts for a 5x5 array of integers and displays the sum of each row and column
+int main(void)
+{
+	int rowSum[R] = {0};
+	int columnSum[C] = {0};
+	int n = 0;
+
+	printf("Enter %d numbers in each row\n", C);
+	for (int i = 1; i <= R; i++)
+	{
+		printf("Enter row %d: ", i);
+		while (1)
+		{
+			n = getchar();
+			if (isdigit(n))
+				rowSum[i] += (int)n - 48;
+			else if (n == 32)
+				continue;
+			else if (n == 10)
+				break;
+			else
+			{
+				printf("Invalid entry\n");
+				return 1;
+			}
+		}
+	}
+	printf("Row totals: ");
+	for (int k = 1; k <= R; k++)
+		printf("%d ", rowSum[k]);
+	printf("\n");
+	return 0;
+}
