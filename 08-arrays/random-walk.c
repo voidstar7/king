a51970fa4735e14ead03d80ad63cd86be39491f4
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-23 13:17:21 -0400

    create grid

--- 08-arrays/random-walk.c
+++ 08-arrays/random-walk.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
+#define R 10
+#define C 10
+
 // generates a 10x10 char array initialized to '.'
 // program walks randomly from element to element, using A - Z as labels for elements visited
 // terminates if the walk goes outside the bounds of the grid or is blocked from moving further
 
 int main(void)
 {
+	int i, j;
+
+	char grid[R][C];
+	for (i = 0; i < R; i++)
+		for (j = 0; j < C; j++)
+			grid[i][j] = '.';
+
+	for (i = 0; i < R; i++)
+	{
+		for (j = 0; j < C; j++)
+			printf(" %c", grid[i][j]);
+		printf("\n");
+	}
 }
