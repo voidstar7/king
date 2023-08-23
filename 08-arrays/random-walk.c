073983f2609b39638279451356a775d84a0e21d6
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-23 13:03:59 -0400

    add requirements

---
+++ 08-arrays/random-walk.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// generates a 10x10 char array initialized to '.'
+// program walks randomly from element to element, using A - Z as labels for elements visited
+// terminates if the walk goes outside the bounds of the grid or is blocked from moving further
+
+int main(void)
+{
+}
