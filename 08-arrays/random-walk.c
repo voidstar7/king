315b15204587cb49bdbddaa6f675135f71ade2da
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-24 14:19:25 -0400

    initialize grid with 0s as borders; values 1-100 inside

--- 08-arrays/random-walk.c
+++ 08-arrays/random-walk.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define DEBUG_ENABLED 1
 #define R 10
 #define C 10
 
@@ -12,32 +13,27 @@
 
 int main(void)
 {
-	int i, j;
-	char start = 'A';
+	int i, j, range = 0;
+	int grid[R + 2][C + 2];
 
-	char grid[R][C];
-	for (i = 0; i < R; i++)
-		for (j = 0; j < C; j++)
-			grid[i][j] = '.';
+	// initializes grid values to 0
+	for (i = 0; i < R + 2; i++)
+		for (j = 0; j < C + 2; j++)
+			grid[i][j] = range;
 
-	srand((unsigned)time(NULL));
-	int direction = rand();
-	printf("%d", n % 4);
+	// overlays grid with number range, keeping 0s on outer edges
+	range++;
+	for (i = 1; i < R + 1; i++)
+		for (j = 1; j < C + 1; range++, j++)
+			grid[i][j] = range;
 
-	/*
-	for (i = 0; i < R; i++)
+#ifdef DEBUG_ENABLED
+	// prints grid
+	for (i = 0; i < R + 2; i++)
 	{
-		for (j = 0; j < C; j++)
-		{
-			if (start <= 'Z')
-			{
-				printf(" %c", start);
-				start++;
-			}
-			else
-				printf(" %c", grid[i][j]);
-		}
+		for (j = 0; j < C + 2; j++)
+			printf(" %3d", grid[i][j]);
 		printf("\n");
 	}
-	*/
+#endif
 }
