b4c2160004cc91562c3d2086d32128e088eec371
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-24 17:03:44 -0400

    implement movement switch statement

--- 08-arrays/random-walk.c
+++ 08-arrays/random-walk.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define DEBUG_ENABLED 1
 #define R 10
 #define C 10
-#define UP grid[rowPosition][columnPosition] - R
-#define RIGHT grid[rowPosition][columnPosition] + 1
-#define DOWN grid[rowPosition][columnPosition] + R
-#define LEFT grid[rowPosition][columnPosition] - 1
+#define UP grid[rowPosition - 1][columnPosition]
+#define RIGHT grid[rowPosition][columnPosition + 1]
+#define DOWN grid[rowPosition + 1][columnPosition]
+#define LEFT grid[rowPosition][columnPosition - 1]
 
 // generates a 10x10 char array initialized to '.'
 // program walks randomly from element to element, using A - Z as labels for elements visited
 // if the next step is 1) off the grid or 2) on top of an existing letter, try a different direction
-// if all directions are blocked, terminate the program
+// if all directions are blocked\n, terminate the program
 
 int main(void)
 {
-	int i, j, move, letter, range = 100;
+	int i, j, move, rowPosition, columnPosition, letter = 'A', range = 100;
 	int grid[R + 2][C + 2];
 
 	// initialize grid values to 0
@@ -33,8 +34,9 @@ int main(void)
 			grid[i][j] = range;
 
 	// start at top left corner
-	int rowPosition = 1, columnPosition = 1;
-	grid[rowPosition][columnPosition] = 'A';
+	rowPosition = 1, columnPosition = 1;
+	grid[rowPosition][columnPosition] = letter;
+	letter++;
 
 	// seed rng
 	srand((unsigned)time(NULL));
@@ -48,23 +50,101 @@ int main(void)
 		if (upBlocked == true && rightBlocked == true && downBlocked == true && leftBlocked == true)
 		{
 		#ifdef DEBUG_ENABLED
-			printf("Movement is blocked\n");
+			printf("Movement is blocked. Exiting after %d/25 moves\n", i);
 		#endif
 			return 1;
 		}
 		move = rand() % 4;
+		#ifdef DEBUG_ENABLED
+		if (move == 0)
+			printf("Attempting to move Up\n");
+		else if (move == 1)
+			printf("Attempting to move Right\n");
+		else if (move == 2)
+			printf("Attempting to move Down\n");
+		else
+			printf("Attempting to move Left\n");
+		#endif
 		switch (move)
 		{
-			/*
-			case 0: // up
-			case 1: // right
-			case 2: // down
-			case 3: // left
-			*/
+			case 0:
+				if (UP == 0 || (UP >= 65 && UP <= 90))
+				{
+				#ifdef DEBUG_ENABLED
+					printf("Up is blocked\n");
+				#endif
+					upBlocked = true;
+					i--;
+					break;
+				}
+				#ifdef DEBUG_ENABLED
+					printf("Successfully moved Up (%i/25)\n", i);
+				#endif
+				UP = letter;
+				letter++;
+				rowPosition--;
+				upBlocked = false; rightBlocked = false; downBlocked = false; leftBlocked = false;
+				break;
+			case 1:
+				if (RIGHT == 0 || (RIGHT >= 65 && RIGHT <= 90))
+				{
+				#ifdef DEBUG_ENABLED
+					printf("Right is blocked\n");
+				#endif
+					rightBlocked = true;
+					i--;
+					break;
+				}
+				#ifdef DEBUG_ENABLED
+					printf("Successfully moved Right (%i/25)\n", i);
+				#endif
+				RIGHT = letter;
+				letter++;
+				columnPosition++;
+				upBlocked = false; rightBlocked = false; downBlocked = false; leftBlocked = false;
+				break;
+			case 2:
+				if (DOWN == 0 || (DOWN >= 65 && DOWN <= 90))
+				{
+				#ifdef DEBUG_ENABLED
+					printf("Down is blocked\n");
+				#endif
+					downBlocked = true;
+					i--;
+					break;
+				}
+				#ifdef DEBUG_ENABLED
+					printf("Successfully moved Down (%i/25)\n", i);
+				#endif
+				DOWN = letter;
+				letter++;
+				rowPosition++;
+				upBlocked = false; rightBlocked = false; downBlocked = false; leftBlocked = false;
+				break;
+			case 3:
+				if (LEFT == 0 || (LEFT >= 65 && LEFT <= 90))
+				{
+				#ifdef DEBUG_ENABLED
+					printf("Left is blocked\n");
+				#endif
+					leftBlocked = true;
+					i--;
+					break;
+				}
+				#ifdef DEBUG_ENABLED
+					printf("Successfully moved Left (%i/25)\n", i);
+				#endif
+				LEFT = letter;
+				letter++;
+				columnPosition--;
+				upBlocked = false; rightBlocked = false; downBlocked = false; leftBlocked = false;
+				break;
+			default:
+				printf("Unexpected error\n");
+				return 0;
 		}
 	}
 
-#ifdef DEBUG_ENABLED
 	// print grid
 	for (i = 0; i < R + 2; i++)
 	{
@@ -72,5 +152,4 @@ int main(void)
 			printf(" %4d", grid[i][j]);
 		printf("\n");
 	}
-#endif
 }
