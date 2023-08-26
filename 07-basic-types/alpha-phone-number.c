f4004803d65c829c3f4df1a20a63b15224257787
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-26 18:46:49 -0400

    store input in array

--- 07-basic-types/alpha-phone-number.c
+++ 07-basic-types/alpha-phone-number.c
@@ -1,34 +1,66 @@
 #include <stdio.h>
+#include <ctype.h>
+
+#define LENGTH 15
 
 // Converts an alphabetic phone number into a numeric one. Assumes that input is in upper case letters. If the original phone number contains digits or punctuation, leaves them unchanged.
 
 int main(void)
 {
 	char c;
+	char number[LENGTH];
+	int i;
 	printf("Enter a phone number: ");	
-	while (1)
+	for (i = 0; i < LENGTH; i++)
 	{
 		c = getchar();
-		if (c >= 65 && c <= 90)
+		if (isalpha(c) || c == '-')
 		{
 			switch (c)
 			{
-				case 'A': case 'B': case 'C':	printf("2"); break;
-				case 'D': case 'E': case 'F':	printf("3"); break;
-				case 'G': case 'H': case 'I':	printf("4"); break;
-				case 'J': case 'K': case 'L':	printf("5"); break;
-				case 'M': case 'N': case 'O':	printf("6"); break;
-				case 'P': case 'Q': case 'R':	printf("7"); break;
-				case 'S': case 'T': case 'U':	printf("8"); break;
-				case 'V': case 'W': case 'X':	printf("9"); break;
-				case 'Y': case 'Z':	printf("0"); break;
+				case 'A': case 'B': case 'C':	
+					number[i] = '2'; 
+					break;
+				case 'D': case 'E': case 'F':
+					number[i] = '3'; 
+					break;
+				case 'G': case 'H': case 'I':
+					number[i] = '4'; 
+					break;
+				case 'J': case 'K': case 'L':
+					number[i] = '5'; 
+					break;
+				case 'M': case 'N': case 'O':
+					number[i] = '6'; 
+					break;
+				case 'P': case 'Q': case 'R':
+					number[i] = '7'; 
+					break;
+				case 'S': case 'T': case 'U':
+					number[i] = '8'; 
+					break;
+				case 'V': case 'W': case 'X':
+					number[i] = '9'; 
+					break;
+				case 'Y': case 'Z':
+					number[i] = '0'; 
+					break;
+				case '-':
+					number[i] = '-';
 			}
+		}
+		else if (isdigit(c))
+		{
+			number[i] = (int)c;
 		}
 		else if (c == '\x0a')
 			break;
 		else
-			printf("%c", c);
+			continue;
 	}
+	printf("In numeric form: ");
+	for (i = 0; i < LENGTH; i++)
+		printf("%c", number[i]);
 	printf("\n");
 	return 0;
 }
