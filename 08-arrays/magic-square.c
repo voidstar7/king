eab3a40e893afe9f862dabbe32ffcdf73dbe047c
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-29 17:14:14 -0400

    add requirements

--- 08-arrays/magic-square.c
+++ 08-arrays/magic-square.c
@@ -10,5 +10,17 @@ int main(void)
 		printf("Invalid input\n");
 		return 1;
 	}
-		printf("valid input\n");
+	/*
+	 place first number in the middle of row 0
+	 place subsequent numbers up 1 row and right 1 column
+		 if the destination is above first row, wrap to last row
+		 if the destination is to right of the last column, wrap to first column
+		 if the destination is occupied, place the next number immediately below the last one 
+
+	 17  24   1   8  15
+	 23   5   7  14  16
+	  4   6  13  20  22
+	 10  12  19  21   3
+	 11  18  25   2   9
+ */
 }
