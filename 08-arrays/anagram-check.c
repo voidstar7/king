6d76fd263eca498bc590979ed494e5c65ff079ee
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-29 15:22:14 -0400

    prompts for 2nd word and decrements array

--- 08-arrays/anagram-check.c
+++ 08-arrays/anagram-check.c
@@ -7,7 +7,7 @@ int main(void)
 	int i;
 	int firstWord[26] = { 0 };
 
-	printf("Enter a word: ");
+	printf("Enter first word: ");
 	while (c != 10)
 	{
 		c = getchar();
@@ -15,6 +15,17 @@ int main(void)
 			continue;
 		c = tolower(c);
 		firstWord[c - 'a'] += 1;
+	}
+	c = 0; // clears newline from input buffer
+	printf("Enter second word: ");
+	while (c != 10)
+	{
+		c = getchar();
+		if (!isalpha(c))
+			continue;
+		c = tolower(c);
+		if (firstWord[c - 'a'] > 0)
+			firstWord[c - 'a'] -= 1;
 	}
 	for (i = 0; i < 26; i++)
 		printf("%d ", firstWord[i]);
