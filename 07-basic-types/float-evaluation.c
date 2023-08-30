ab4b9271f3706f0284aa4e749e65af708c0b6632
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-30 17:16:19 -0400

    create simple addition loop

--- 07-basic-types/float-evaluation.c
+++ 07-basic-types/float-evaluation.c
@@ -1,85 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <stdbool.h>
+
+// INCOMPLETE
 
 // evaluates an expression of from left to right regardless of operator precedence. Possible operators are +, -, *, and /
 
 int main(void)
 {
-	bool number, decimal;
-	int flag = 0, scale = 0;
-	float digit = 0.0f, value = 0.0f, oldNumber = 0.0f;
+	int i;
 	char c;
+	int value = 0;
+	
 	printf("Enter an expression: ");
-
-input:
 	while (1)
 	{
+		scanf("%d", &i);
+		value += i;
 		c = getchar();
-		if (isdigit(c)) // number
-		{
-			number = true;
-			flag = 0;
-			scale++;
+		if (c == 10)
 			break;
-		}
-		switch (c)
-		{
-			case 10: // return
-				goto end;
-			case 32: // space
-				scale = 0; 
-				continue;
-			case 46: // decimal
-				decimal = true;
-				scale++;
-				break;
-			case 43: // add
-				flag = 1;
-				scale = 0;
-				oldNumber = digit;
-				number = false;
-				break;
-			case 45: // subtract
-				flag = 2; 
-				scale = 0;
-				break;
-			case 42: // multiply
-				flag = 3; 
-				scale = 0;
-				break;
-			case 47: // divide
-				flag = 4;
-				scale = 0;
-				break;
-			default:
-				continue;
-		}
 	}
-	if (number)
-	{
-		if (scale == 1)
-		{
-			digit = (float)c - 48;
-			goto input;
-		}
-		digit = (digit * 10) + ((float)c - 48);
-		goto input;
-	}
-	if (flag)
-	{
-		if (flag == 1)
-		{
-			value = oldNumber + digit;
-			goto input;
-		}
-		return 1;
-	}
-	return 1;
-
-end:
-		printf("Old number: %f\n", oldNumber);
-		printf("Digit: %f\n", digit);
-		printf("Value: %f\n", value);
-		return 0;
+	printf("%d", value);
 }
