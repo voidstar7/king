d9549d12fe3a98a73a43819d5f2d1430d94c456e
ex07 <119447705+ex07@users.noreply.github.com>
2023-08-31 22:40:09 -0400

    shift message; print ciphertext

--- 08-arrays/caesar-cipher.c
+++ 08-arrays/caesar-cipher.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
-	int shift, length = 0;
+	int i, shift, length = 0;
 	char c;
-	char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 	char message[80];
+	char cipher[80];
 
 	printf("Enter message (max 80 chars): ");
 	while (1)
@@ -18,4 +19,24 @@ int main(void)
 	}
 	printf("Enter shift amount (1-25): ");
 	scanf("%d", &shift);
+
+	for (i = 0; i < length; i++)
+	{
+		if (isalpha(message[i])) 
+		{
+			if (isupper(message[i]))
+			{
+				cipher[i] = ((message[i] - 'A') + shift) % 26 + 'A';
+				continue;
+			}
+			cipher[i] = ((message[i] - 'a') + shift) % 26 + 'a';
+			continue;
+		}
+		cipher[i] = message[i];
+	}
+	printf("Encrypted message: ");
+	for (i = 0; i < length; i++)
+		printf("%c", cipher[i]);
+	printf("\n");
+	return 0;
 }
