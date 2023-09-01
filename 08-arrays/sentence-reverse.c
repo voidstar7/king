455e699ab09aad69aeec1bf435a89fd2b173f3e3
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-01 10:08:09 -0400

    print all words in reverse except first

--- 08-arrays/sentence-reverse.c
+++ 08-arrays/sentence-reverse.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define debug
+
+// incomplete
 int main(void)
 {
 	char sentence[100];
 	char reverseSentence[100];
 	char i, j, input;
-	int length = 0, wordLength = 0, reverseIndex = 0;
+	int length = 0, wordLength = 0, reverseIndex = 0, wordStart;
 
 	printf("Enter a sentence: ");
 	while (1)
 	{
 		input = getchar();
-		if (input == 10)
+		if (input == '\x0a')
 			break;
 		sentence[length] = input;
 		length++;
 	}
 	for (i = length - 1; i >= 0; i--)
 	{
-		printf("sentence[%d] is (%c)\n", i, sentence[i]);
 		if (isalpha(sentence[i])) 
 		{
 			wordLength++;
-			printf("word length is %d\n", wordLength);
+#ifdef debug
+		printf("sentence[%d] is %c word length %d\n", i, sentence[i], wordLength);
+#endif
 			continue;
 		}
-		else if (sentence[i] == 32)
+		else if (sentence[i] == '\x20')
 		{
-			for (j = reverseIndex; j < wordLength; j++)
+			wordStart = i + 1;
+			for (j = 0; j < wordLength; j++)
 			{
-				reverseSentence[reverseIndex] = sentence[(i + 1) - reverseIndex];
-				printf("reverseSentence[%d] is (%c)\n", j, reverseSentence[reverseIndex]);
+				reverseSentence[reverseIndex] = sentence[wordStart + j];
 				reverseIndex++;
 			}
+			reverseSentence[reverseIndex] = '\x20';
+			reverseIndex++;
 			wordLength = 0;
-			printf("word length is %d\n", wordLength);
 		}
 	}
+	for (i = 0; i < length; i++)
+		printf("%c", reverseSentence[i]);
 }
