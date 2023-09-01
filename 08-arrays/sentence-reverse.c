08d1628de3748322c01fa2bd611ef7fcc13e4a80
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-01 15:42:25 -0400

    handle punctuation; remove debug statements

--- 08-arrays/sentence-reverse.c
+++ 08-arrays/sentence-reverse.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-#define debug
-
-// incomplete
 int main(void)
 {
 	char sentence[100];
 	char reverseSentence[100];
-	char i, j, input;
+	char i, j, input, terminator;
+	bool hasTerminator;
 	int length = 0, wordLength = 0, reverseIndex = 0, wordStart;
 
 	sentence[0] = '\x02'; // assign STX to first element
 	length++;
-
 	printf("Enter a sentence: ");
 	while (1)
 	{
@@ -23,14 +21,16 @@ int main(void)
 		sentence[length] = input;
 		length++;
 	}
-	for (i = length - 1; i >= 0; i--)
+	if (ispunct(sentence[length - 1]))
 	{
-		if (isalpha(sentence[i])) 
+		terminator = sentence[length - 1];
+		hasTerminator = true;
+	}
+	for (i = length; i >= 0; i--)
+	{
+		if (isalpha(sentence[i]) || ispunct(sentence[i])) 
 		{
 			wordLength++;
-#ifdef debug
-		printf("sentence[%d] is %c word length %d\n", i, sentence[i], wordLength);
-#endif
 			continue;
 		}
 		else if (sentence[i] == '\x20')
@@ -55,7 +55,13 @@ int main(void)
 			}
 		}
 	}
+	printf("Reversal of sentence: ");
 	for (i = 0; i < length; i++)
+	{
+		if (reverseSentence[i] == terminator && reverseSentence[i + 1] == '\x20')
+			continue;
 		printf("%c", reverseSentence[i]);
+	}
+	printf("%c", terminator);
 	printf("\n");
 }
