cd32da98e85f73c386431e2abc9213fe278613f2
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-09 13:05:20 -0400

    break main up into functions

--- 08-arrays/anagram-check.c
+++ 08-arrays/anagram-check.c
@@ -2,43 +2,44 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+void read_word(int counts[26]);
+bool equal_array(int counts1[26], int counts2[26]);
+
 int main(void)
 {
-	char c;
-	int i;
-	bool firstWordEntered = false;
-	int word[26] = { 0 };
+	int counts1[26] = { 0 };
+	int counts2[26] = { 0 };
 
 	printf("Enter first word: ");
-input:
+	read_word(counts1);
+
+	printf("Enter second word: ");
+	read_word(counts2);
+
+	if (equal_array(counts1, counts2))
+		printf("The words are anagrams\n");
+	else
+		printf("The words are not anagrams\n");
+	return 0;
+}
+
+void read_word(int counts[26])
+{
+	char c = 0;
 	while (c != 10)
 	{
 		c = getchar();
 		if (!isalpha(c))
 			continue;
 		c = tolower(c);
-		if (firstWordEntered == false)
-		{
-			word[c - 'a'] += 1;
-			continue;
-		}
-		word[c - 'a'] -= 1;
+		counts[c - 'a'] += 1;
 	}
-	if (firstWordEntered == false)
-	{
-		c = 0; // clears newline from input buffer
-		firstWordEntered = true;
-		printf("Enter second word: ");
-		goto input;
-	}
-	for (i = 0; i < 26; i++)
-	{
-		if (word[i] != 0)
-		{
-			printf("The words are not anagrams\n");
-			return 0;
-		}
-	}
-	printf("The words are anagrams\n");
-	return 0;
+}
+
+bool equal_array(int counts1[26], int counts2[26])
+{
+	for (int i = 0; i < 26; i++)
+		if (counts1[i] != counts2[i])
+			return false;
+		return true;
 }
