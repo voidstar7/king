6c5deed4f955882347a567a1e103e1faf69a9505
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-09 21:23:40 -0400

    add requirements and function prototypes

---
+++ 09-functions/craps.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// on the first roll, the player wins if the sum is 7 or 11
+// they lose if the sum is 2, 3, or 12
+// any other roll becomes the point and the game continues
+// on subsequent rolls, the player wins if they roll the point
+// they lose by rolling a 7
+// the game continues with any other rolls
+// when a game is over, the program records whether or not it was won or lost
+// the user is asked if they want to play again (y / n)
+// if yes, a new game is initiated
+// if no, the number of games won and lost is displayed, then the program exits
+
+int roll_dice(void);
+int play_game(void);
+
+int main(void)
+{
+
+
+}
