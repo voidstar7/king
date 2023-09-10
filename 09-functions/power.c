18e5d58d5859de6878f4120ffc629f6068f9215b
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-10 19:33:55 -0400

    separate into odd/even functions

--- 09-functions/power.c
+++ 09-functions/power.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 
-int power(int x, int n);
+int power_even(int x, int n);
+int power_odd(int x, int n);
 
 int main(void)
 {
 	int x, n;
 	printf("Enter a value for x: "); scanf("%d", &x);
 	printf("Enter a value for n: "); scanf("%d", &n);
-	printf("%d^%d = %d\n", x, n, power(x, n));
+	if (n % 2 == 1)
+	{
+		printf("%d^%d = %d\n", x, n, power_odd(x, n));
+		return 0;
+	}
+	printf("%d^%d = %d\n", x, n, power_even(x, n));
 	return 0;
 }
 
-// x^n = x * x^n - 1
-int power(int x, int n)
+// if n is odd: x^n = x * x^n-1
+int power_odd(int x, int n)
+{
+	if (n == 0)
+		return 1;
+	else 
+		return x * power_odd(x, n - 1);
+}
+
+// if n is even: x^n = (x^n/2)^2
+int power_even(int x, int n)
 {
 	if (n == 0)
 		return 1;
 	else
-		return x * power(x, n - 1);
+	{
+		int y = power_odd(x, n / 2);
+		return y * y;
+	}
 }
