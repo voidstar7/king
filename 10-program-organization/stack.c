71cfd832e5ba1a0155484a849c1395860fe320d4
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-12 22:08:50 -0400

    add prototypes; add overflow/underflow functions; test with ints

--- 10-program-organization/stack.c
+++ 10-program-organization/stack.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-#define STACK_SIZE 100
+#define STACK_SIZE 5 
 
-// global
+void stack_overflow(void);
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(int i);
+int pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
+
 int contents[STACK_SIZE];
 int top = 0;
+
+int main(void)
+{
+	int n, count;
+	char c = 0;
+
+	make_empty();
+	printf("Enter numbers to push onto the stack: ");
+	while (c != '\x0a')
+	{
+		scanf("%d", &n);
+		push(n);
+		printf("Pushed %d onto stack\n", n);
+		c = getchar();
+	}
+	printf("*** Top is %d ***\n", top);
+	for (int i = 0; i < STACK_SIZE; i++)
+		printf("[%d]%d ", i, contents[i]);
+	printf("\n");
+	for (int i = 0; i < STACK_SIZE; i++)
+	{
+		printf("Popped %d off stack\n", pop());
+	}
+	printf("*** Top is %d ***\n", top);
+	if (is_empty())
+		printf("Stack is empty\n");
+	return 0;
+}
 
 void make_empty(void)
 {
@@ -24,7 +61,7 @@ bool is_full(void)
 
 void push(int i)
 {
-	if (isfull())
+	if (is_full())
 		stack_overflow();
 	else
 		contents[top++] = i;
@@ -33,7 +70,20 @@ void push(int i)
 int pop(void)
 {
 	if (is_empty())
+	{
 		stack_underflow();
+		return 0;
+	}
 	else
-		return content[--top];
+		return contents[--top];
+}
+
+void stack_overflow(void)
+{
+	printf("Stack overflow\n");
+}
+
+void stack_underflow(void)
+{
+	printf("Stack underflow\n");
 }
