99be415ad815bb9c837c3a3c9de4bd0c1869c665
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-16 19:31:32 -0400

    create over/underflow(); handle adding 2 ints

--- 10-program-organization/rpn.c
+++ 10-program-organization/rpn.c
@@ -13,6 +13,8 @@ bool is_empty(void);
 bool is_full(void);
 void push(int i);
 int pop(void);
+void stack_underflow(void);
+void stack_overflow(void);
 
 char stack[STACK_SIZE];
 int top = 0; 
@@ -29,15 +31,21 @@ int main(void)
 		else if (ispunct(pushed))
 		{
 			calculate(pushed);
-			printf("total = %d\n", total);
-			pop();
 			for (int i = 0; i < STACK_SIZE; i++)
 				printf("[%d]%c ", i, stack[i]);
 			printf("\n");
-			return 0;
+			continue;
 		}
-		else
-			return 0;
+		else if (pushed == 'p')
+		{
+			if (is_empty())
+			{
+				printf("total = %d\n", total);
+				return 0;
+			}
+			stack_underflow();
+			return 1;
+		}
 	}
 }
 
@@ -47,9 +55,11 @@ char prompt(void)
 	char input;
 	printf(">>> ");
 	scanf(" %c", &input);
+	if (input == 'p')
+		return 'p';
 	push(input);
-	printf("Pushed %d onto stack (top: %d)\n", input, top);
-
+	printf("Pushed %d onto stack (top: %d)\n",
+			input - 48, top);
 	for (int i = 0; i < STACK_SIZE; i++)
 		printf("[%d]%c ", i, stack[i]);
 	printf("\n");
@@ -64,6 +74,8 @@ void calculate(char pushed)
 			total = 
 				(stack[top - 2] - 48)+
 				(stack[top - 3] - 48);
+			pop();
+			printf("+ detected (top: %d)\n", top);
 			break;
 		case '-':
 			total = 
@@ -81,7 +93,8 @@ void calculate(char pushed)
 				(stack[top - 3] - 48);
 			break;
 		default:
-			exit(1);
+			printf("Error: illegal char pushed\n");
+			exit(EXIT_FAILURE);
 	}
 }
 
@@ -99,8 +112,8 @@ void push(int i)
 {
 	if (is_full())
 	{
-		printf("Stack overflow\n");
-		exit(1);
+		stack_overflow();
+		exit(EXIT_FAILURE);
 	}
 	else
 		stack[top++] = i;
@@ -111,14 +124,26 @@ int pop(void)
 	int i;
 	if (is_empty())
 	{
-		printf("Stack underflow\n");
-		exit(1);
+		stack_underflow();
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		for (i = 1; i <= 3; i++)
+		{
 			stack[top - i] = 0;
+		}
+		top -= 3;
 		return top;
 	}
 }
 
+void stack_underflow(void)
+{
+	printf("Error: stack underflow\n");
+}
+
+void stack_overflow(void)
+{
+	printf("Error: stack overflow\n");
+}
