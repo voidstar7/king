31893c0e61b0d2ada5c3b681acd213db1140168d
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-25 21:03:20 -0400

    add function prototypes and visual example

---
+++ 10-program-organization/seven-segment-display.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+			 0	
+			___
+	5	 |   |  1
+		 |_6_|
+  	 |   |  
+  4	 |___|  2
+			 3
+
+ ___
+|   |
+|___|
+|   |
+|___|
+
+*/
+
+void clear digits_array(void);
+void process_digit(int digit, int position);
+void print_digits_array(void);
+
+int main(void)
+{
+	...
+}
