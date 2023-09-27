9a8b546dde0fc4305875463a51868a07067b107c
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-26 20:03:37 -0400

    print 8 using 3x3 array

--- 10-program-organization/seven-segment-display.c
+++ 10-program-organization/seven-segment-display.c
@@ -9,19 +9,39 @@
   4	 |___|  2
 			 3
 
- ___
-|   |
-|___|
-|   |
-|___|
+ _ 
+|_|
+|_|
 
 */
 
-void clear digits_array(void);
-void process_digit(int digit, int position);
-void print_digits_array(void);
+//void clear digits_array(void);
+//void process_digit(int digit, int position);
+//void print_digits_array(void);
 
 int main(void)
 {
-	...
+	char a[3][3];
+	int i, j;
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (i == 0 && (j == 0 || j == 2))
+				a[i][j] = ' ';
+			else if (j == 1)
+				a[i][j] = '_';
+			else
+				a[i][j] = '|';
+	
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			printf("%c", a[i][j]);
+		printf("\n");
+	}
 }
+/*
+ _ 
+|_|
+|_|
+
+*/
