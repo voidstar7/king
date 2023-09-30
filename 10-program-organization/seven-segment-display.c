d493e25251b917eb64b04b71e5e0fce389810e90
ex07 <119447705+ex07@users.noreply.github.com>
2023-09-29 22:27:03 -0400

    print 3 and 5 side by side by manually assigning array elements

--- 10-program-organization/seven-segment-display.c
+++ 10-program-organization/seven-segment-display.c
@@ -11,7 +11,7 @@
 //void process_digit(int digit, int position);
 //void print_digits_array(void);
 
-char digits[10][4];
+char digits[3][27];
 char segments[10][3][3] =
 {
 	{ { ' ', '_', ' ' }, { '|', ' ', '|' }, { '|', '_', '|' } }, // 0
@@ -28,6 +28,7 @@ char segments[10][3][3] =
 
 int main(void)
 {
+	/*
 	char input[10], ch;
 	int i, count = 0;
 	printf("Enter digits (10 max): ");
@@ -43,6 +44,39 @@ int main(void)
 			break;
 		input[count] = ch;
 		count++;
+	}
+	*/
+	int i, j;
+
+	digits[0][0] = segments[3][0][0];
+	digits[0][1] = segments[3][0][1];
+	digits[0][2] = segments[3][0][2];
+
+	digits[1][0] = segments[3][1][0];
+	digits[1][1] = segments[3][1][1];
+	digits[1][2] = segments[3][1][2];
+
+	digits[2][0] = segments[3][2][0];
+	digits[2][1] = segments[3][2][1];
+	digits[2][2] = segments[3][2][2];
+
+	digits[0][3] = segments[5][0][0];
+	digits[0][4] = segments[5][0][1];
+	digits[0][5] = segments[5][0][2];
+
+	digits[1][3] = segments[5][1][0];
+	digits[1][4] = segments[5][1][1];
+	digits[1][5] = segments[5][1][2];
+
+	digits[2][3] = segments[5][2][0];
+	digits[2][4] = segments[5][2][1];
+	digits[2][5] = segments[5][2][2];
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 27; j++)
+			printf("%c", digits[i][j]);
+		printf("\n");
 	}
 }
 
