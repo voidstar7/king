34f566b1b757c45af6d74d58db0ea5bbfa769768
ex07 <119447705+ex07@users.noreply.github.com>
2023-11-05 21:18:32 -0500

    check input range; capitalize strings

--- 05-selection-statements/number-to-word.c
+++ 05-selection-statements/number-to-word.c
@@ -6,15 +6,17 @@ int main(void)
 {
 	char *oneToNine[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 	char *elevenToNineteen[] = { "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-	char *twentyToNinety[] = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+	char *twentyToNinety[] = { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
 	char *s[25];
 
 	int number;
 	while (1)
 	{
 		printf("Enter a two digit number: "); 
-		scanf("%2d", &number);
-		if (number == 10)
+		scanf("%d", &number);
+		if (number < 10 || number > 99)
+			continue;
+		else if (number == 10)
 		{
 			printf("Ten\n");
 			break;
