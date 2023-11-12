c143d0418e6be1355be56e18074516a65b591f4b
ex07 <119447705+ex07@users.noreply.github.com>
2023-11-11 19:25:56 -0500

    use string function to shift chars

--- 08-arrays/caesar-cipher.c
+++ 08-arrays/caesar-cipher.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define MAX 80
+
+void encrypt(char *message, int shift);
+
 int main(void)
 {
-	int i, shift, length = 0;
-	char c;
-	char message[80];
-	char cipher[80];
+	char message[MAX];
+	int shift;
 
 	printf("Enter message (max 80 chars): ");
-	while (1)
-	{
-		c = getchar();
-		if (c == '\x0a')
-			break;
-		message[length] = c;
-		length++;
-	}
+	fgets(message, MAX, stdin);
+
 	printf("Enter shift amount (1-25): ");
 	scanf("%d", &shift);
+	encrypt(message, shift);
+	printf("%s", message);
 
-	for (i = 0; i < length; i++)
-	{
-		if (isalpha(message[i])) 
-		{
-			if (isupper(message[i]))
-			{
-				cipher[i] = ((message[i] - 'A') + shift) % 26 + 'A';
-				continue;
-			}
-			cipher[i] = ((message[i] - 'a') + shift) % 26 + 'a';
-			continue;
-		}
-		cipher[i] = message[i];
-	}
-	printf("Encrypted message: ");
-	for (i = 0; i < length; i++)
-		printf("%c", cipher[i]);
-	printf("\n");
 	return 0;
 }
+
+void encrypt(char *message, int shift)
+{
+	while (*message)
+	{
+		if (isalpha(*message)) 
+		{
+			if (isupper(*message))
+				*message = ((*message - 'A') + shift) % 26 + 'A';
+			else
+				*message = ((*message - 'a') + shift) % 26 + 'a';
+		}
+		message++;
+	}
+}
+
