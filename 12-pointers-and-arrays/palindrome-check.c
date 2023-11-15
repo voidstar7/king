e1f1028bad263fe03f206ee60e4af5e99a6bea32
ex07 <119447705+ex07@users.noreply.github.com>
2023-11-14 22:20:59 -0500

    pass string to function

--- 12-pointers-and-arrays/palindrome-check.c
+++ 12-pointers-and-arrays/palindrome-check.c
@@ -2,41 +2,39 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-bool palindrome_check(char message[], int len);
+#define MAX 128
+
+bool is_palindrome(char *message);
 
 int main(void)
 {
-	int max = 100, len = 0, i;
-	char message[max], c;
+	char message[MAX];
 
 	printf("Enter a message: ");
-	while (1)
-	{
-		c = getchar();
-		if (c == '\n' || len > max)
-			break;
-		else if (!isalpha(c))
-			continue;
-		message[len++] = tolower(c);
-	}
-	if (palindrome_check(message, len))
+	fgets(message, MAX, stdin);
+
+	message[strlen(message) - 1] = '\0';
+	for (int i = 0; i < strlen(message); i++)
+		message[i] = tolower(message[i]);
+
+	if (is_palindrome(message))
 		printf("Message is a palindrome\n");
 	else
 		printf("Message is not a palindrome\n");
 	return 0;
 }
 
-bool palindrome_check(char message[], int len)
+bool is_palindrome(char *message)
 {
-	char *p = &message[0];
-	bool odd;
-	int mid = len / 2;
-	int end = len - 1;
+	char *p = message;
+	int 
+		len = (int)strlen(message),
+		mid = len / 2,
+		end = len - 1;
 
-	if (len % 2 == 1)
-		odd = true;
-	if (odd)
+	if (len % 2)
 	{
 		while ((p - message) < mid)
 		{
@@ -47,7 +45,6 @@ bool palindrome_check(char message[], int len)
 		}
 		return true;
 	}
-	// else if len = even
 	while ((p - message) <= mid)
 	{
 		if (*p == *(message + end--))
