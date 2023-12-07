e4f45e0a3554c51421d5d2f1583ee87f1271ba63
ex07 <119447705+ex07@users.noreply.github.com>
2023-12-06 21:52:19 -0500

    add function description

--- 15-writing-large-programs/justify/word.h
+++ 15-writing-large-programs/justify/word.h
@@ -1,6 +1,7 @@
 #ifndef WORD_H
 #define WORD_H
 
+// reads the next word from the input and stores it in word. Makes word empty if no word could be read because of EOF. Truncates the word if its length exceeds len.
 void read_word(char *word, int len);
 
 #endif
