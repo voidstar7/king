1d629a6dda96a957a9103d47fc29eaee022c8280
ex07 <119447705+ex07@users.noreply.github.com>
2023-12-09 21:20:04 -0500

    move functions and globals to header file

--- 10-program-organization/stack/stack.c
+++ 10-program-organization/stack/stack.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-#define DEBUG
-#define STACK_SIZE 100 
-
-void stack_overflow(void);
-void make_empty(void);
-bool is_empty(void);
-bool is_full(void);
-void push(char c);
-int pop(void);
-void stack_overflow(void);
-void stack_underflow(void);
-
-int contents[STACK_SIZE];
-int top = 0;
+#include "stack.h"
 
 int main(void)
 {
@@ -59,50 +45,4 @@ int main(void)
 	}
 	printf("Parentheses/braces are nested correctly\n");
 	return 0;
-}
-
-void make_empty(void)
-{
-	top = 0;
-}
-
-bool is_empty(void)
-{
-	return top == 0;
-}
-
-bool is_full(void)
-{
-	return top == STACK_SIZE;
-}
-
-void push(char c)
-{
-	if (is_full())
-		stack_overflow();
-	else
-		contents[top++] = c;
-}
-
-int pop(void)
-{
-	if (is_empty())
-	{
-		stack_underflow();
-		return 0;
-	}
-	else
-		return contents[--top];
-}
-
-void stack_overflow(void)
-{
-	printf("Stack overflow\n");
-	exit(1);
-}
-
-void stack_underflow(void)
-{
-	printf("Parentheses/braces are NOT nested correctly\n");
-	exit(1);
 }
---
+++ 10-program-organization/stack/stack.h
@@ -0,0 +1,58 @@
+#define STACK_SIZE 100 
+
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(char c);
+int pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
+
+int top = 0;
+int contents[STACK_SIZE];
+
+void make_empty(void)
+{
+	top = 0;
+}
+
+bool is_empty(void)
+{
+	return top == 0;
+}
+
+bool is_full(void)
+{
+	return top == STACK_SIZE;
+}
+
+void push(char c)
+{
+	if (is_full())
+		stack_overflow();
+	else
+		contents[top++] = c;
+}
+
+int pop(void)
+{
+	if (is_empty())
+	{
+		stack_underflow();
+		return 0;
+	}
+	else
+		return contents[--top];
+}
+
+void stack_overflow(void)
+{
+	printf("Stack overflow\n");
+	exit(1);
+}
+
+void stack_underflow(void)
+{
+	printf("Parentheses/braces are NOT nested correctly\n");
+	exit(1);
+}
