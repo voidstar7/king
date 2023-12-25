914fe5f4289d064eb5ac5d42b3eca1c6f5b8a4e4
ex07 <119447705+ex07@users.noreply.github.com>
2023-12-24 20:24:17 -0500

    check if coordinates are within rectangle

--- 16-structures-unions-enums/point-rectangle.c
+++ 16-structures-unions-enums/point-rectangle.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 struct point { int x, y; };
 struct rectangle { struct point upperLeft, lowerRight; };
 
 void operate(struct rectangle r);
 struct rectangle shift(struct rectangle r, int x, int y);
+bool check_coordinates(struct rectangle s, struct point isInRectangle);
 
 int main(void)
 {
-	struct point ul, lr;
+	struct point ul, lr, isInRectangle;
 	struct rectangle r;
 
 	printf("Upper left (x): ");
@@ -28,7 +30,7 @@ int main(void)
 void operate(struct rectangle r)
 {
 	int width, height, upDown, rightLeft;
-	struct point center;
+	struct point center, isInRectangle;
 	struct rectangle s;
 
 	width = r.lowerRight.x - r.upperLeft.x;
@@ -52,6 +54,13 @@ void operate(struct rectangle r)
 	printf("Shifted rectangle upper left: (%d, %d)\nShifted rectangle lower right: (%d, %d)\n",
 			s.upperLeft.x, s.upperLeft.y,
 			s.lowerRight.x, s.lowerRight.y);
+
+	printf("Enter coordinates (x, y): ");
+	scanf("%d, %d", &isInRectangle.x, &isInRectangle.y);
+	if (check_coordinates(s, isInRectangle))
+		printf("Coordinates are within the shifted rectangle\ns");
+	else
+		printf("Coordinates are not within the shifted rectangle\n");
 }
 
 struct rectangle shift(struct rectangle r, int x, int y)
@@ -62,3 +71,12 @@ struct rectangle shift(struct rectangle r, int x, int y)
 	r.lowerRight.y += y;
 	return r;
 }
+
+bool check_coordinates(struct rectangle s, struct point isInRectangle)
+{
+	if (isInRectangle.x > s.lowerRight.x ||
+			isInRectangle.y > s.upperLeft.y)
+		return false;
+	return true;
+}
+
