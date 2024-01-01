72cb6940fa3355c605defef73f2a7173052194bf
ex07 <119447705+ex07@users.noreply.github.com>
2023-12-31 22:35:13 -0500

    update formatting

--- 05-selection-statements/dates.c
+++ 05-selection-statements/dates.c
@@ -2,44 +2,60 @@
 
 // prompts for calendar dates then returns the earliest one
 
-int main(void) {
+int main(void) 
+{
 	int month, day, year, earlyMonth, earlyDay, earlyYear;
-	printf("Enter a date (mm/dd/yy) - 0/0/0 to stop: "); scanf("%d/%d/%d", &month, &day, &year);
-	if (month > 12 || day > 31 || year < 0) { 
+
+	printf("Enter a date (mm/dd/yy) - 0/0/0 to stop: "); 
+	scanf("%d/%d/%d", &month, &day, &year);
+
+	if (month > 12 || day > 31 || year < 0) 
+	{ 
 			printf("Invalid entry\n");
 			return 1;
 	}
-	else if (month == 0 && day == 0 && year == 0) { 
+	else if (month == 0 && day == 0 && year == 0) 
+	{ 
 		goto zeroInit;
 	}	
-	else { 
+	else 
+	{ 
 		earlyMonth = month;
 		earlyDay = day;
 		earlyYear = year;
-		while (1) {
-			printf("Enter a date (mm/dd/yy) - 0/0/0 to stop: "); scanf("%d/%d/%d", &month, &day, &year);
-			if (month > 12 || day > 31 || year < 0) { 
+		while (1) 
+		{
+			printf("Enter a date (mm/dd/yy) - 0/0/0 to stop: "); 
+			scanf("%d/%d/%d", &month, &day, &year);
+			if (month > 12 || day > 31 || year < 0) 
+			{ 
 				printf("Invalid entry\n");
 				return 1;
 			}
 			else if (month == 0 && day == 0 && year == 0) 
 				break;
-			else { 
-				if (year < earlyYear) {
+			else 
+			{ 
+				if (year < earlyYear) 
+				{
 					earlyYear = year;
 					earlyMonth = month;
 					earlyDay = day;
 					continue;
 				}
-				else if (year == earlyYear) { 
-					if (month < earlyMonth) {
+				else if (year == earlyYear) 
+				{ 
+					if (month < earlyMonth) 
+					{
 						earlyYear = year;
 						earlyMonth = month;
 						earlyDay = day;
 						continue;
 					}
-					else if (month == earlyMonth) { 
-						if (day < earlyDay) { 
+					else if (month == earlyMonth) 
+					{ 
+						if (day < earlyDay) 
+						{ 
 							earlyYear = year;
 							earlyMonth = month;
 							earlyDay = day;
