5a6846d1936423ad7dc578c72d917add0a3a507c
ex07 <119447705+ex07@users.noreply.github.com>
2024-01-05 21:56:30 -0500

    add exercise

---
+++ 17-advanced-uses-of-pointers/linked-list.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int value;
+	struct node *next;
+};
+
+struct node *add_to_list(struct node *head, int n);
+struct node *read_numbers(void);
+void print_list(struct node *head);
+
+int main(void)
+{
+	struct node *head = read_numbers();
+	print_list(head);
+}
+
+struct node *read_numbers(void)
+{
+	struct node *head = NULL;
+	int n;
+
+	printf("Enter a series of integers (0 to terminate): ");
+	for (;;) {
+		scanf("%d", &n);
+		if (n == 0)   
+			return head;
+		head = add_to_list(head, n);
+	}
+}
+
+struct node *add_to_list(struct node *head, int n)
+{
+	struct node *newNode;
+
+	newNode = malloc(sizeof(struct node));
+	if (newNode == NULL) {
+		printf("Could not assign memory for new node\n"); 
+		exit(1);
+	}
+	newNode->value = n;
+	newNode->next = head;
+	return newNode;
+}
+
+void print_list(struct node *head)
+{
+	struct node *p;
+
+	for (p = head; p != NULL; p = p->next)
+		printf("%d ", p->value);
+}
