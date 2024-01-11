2cdf6ffa1c52a29f86a3ec103bb69adfcbf877b7
ex07 <119447705+ex07@users.noreply.github.com>
2024-01-10 20:22:03 -0500

    create simple linked list; print from head to tail

--- 17-advanced-uses-of-pointers/linked-list.c
+++ 17-advanced-uses-of-pointers/linked-list.c
@@ -2,82 +2,65 @@
 #include <stdlib.h>
 
 struct node {
-	int value;
-	int position;
+	int data;
 	struct node *next;
 };
 
-struct node *add_to_list(struct node *head, int n);
-struct node *read_numbers(void);
+struct node *create_node(struct node *listItem);
 void print_list(struct node *head);
-struct node *delete_from_list(struct node *list, int n);
 
-int p = 0;
+int numNodes = 0;
 
 int main(void) {
-	int del;
+	struct node *head, *listItem;
 
-	for (;;) {
-		struct node *head = read_numbers();
-		print_list(head);
-		printf("Which node value do you want to delete? ");
-			scanf("%d", &del);
-		delete_from_list(head, del);
-		print_list(head);
-	}
+	// first node = head
+	head = listItem = create_node(listItem);
+	printf("%d %p\n", listItem->data, listItem);
+	printf("%d %p\n", head->data, head);
+
+	listItem = create_node(listItem);
+	printf("%d %p\n", listItem->data, listItem);
+	printf("%d %p\n", head->data, head);
+
+	listItem = create_node(listItem);
+	printf("%d %p\n", listItem->data, listItem);
+	printf("%d %p\n", head->data, head);
+
+	// last node = tail
+	listItem->next = NULL;
+
+	print_list(head);
+	free(listItem);
 }
 
-struct node *read_numbers(void) {
-	struct node *head = NULL;
-	int n;
-
-	printf("Enter a series of integers (0 to terminate): ");
-	for (;;) {
-		scanf("%d", &n);
-		if (n == 0)   
-			return head;
-		head = add_to_list(head, n);
-	}
-}
-
-struct node *add_to_list(struct node *head, int n) {
+struct node *create_node(struct node *listItem) {
 	struct node *newNode;
 
 	newNode = malloc(sizeof(struct node));
 	if (newNode == NULL) {
-		printf("Could not assign memory for new node\n"); 
+		printf("Unable to assign memory to new node\n");
 		exit(1);
 	}
-	newNode->value = n;
-	newNode->position = p;
-	newNode->next = head;
-	p++;
+	printf("Enter node data: ");
+	scanf(" %d", &newNode->data);
+	if (numNodes == 0) {
+		newNode->next = NULL;
+	}
+	else {
+		listItem->next = newNode;
+		newNode->next = NULL;
+	}
+	numNodes++;
 	return newNode;
 }
 
 void print_list(struct node *head) {
 	struct node *p;
-
-	for (p = head; p != NULL; p = p->next)
-		printf("[%d]%d ", p->position, p->value);
-	printf("\n");
+	
+	for (p = head; p->next != NULL; p = p->next)
+		printf("%d\n", p->data);
+	printf("%d\n", p->data);
 }
 
-struct node *delete_from_list(struct node *list, int n) {
-	struct node *cur, *prev;
 
-	for (
-			cur = list;
-			cur != NULL && cur->value != n;
-			cur = cur->next)
-		;
-
-	if (cur == NULL)
-		return list;
-	if (cur + 1 == NULL)
-		list = list->next;
-	else
-		(cur + 1)->next = cur->next;
-	free(cur);
-	return list;
-}
