f190c375cc538fc5245ba7979b6da16d04a1bc3e
ex07 <119447705+ex07@users.noreply.github.com>
2024-01-13 19:44:10 -0500

    implement main switch

--- 17-advanced-uses-of-pointers/stack-linked-list.c
+++ 17-advanced-uses-of-pointers/stack-linked-list.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 10
+#define PRINT_COMMANDS printf("Commands:\n(p) push\n(o) pop\n(t) print\n(e) exit\n")
+
 
 void make_empty(void);
-int is_empty(void);
-int is_full(void);
-void push(int i);
+bool is_empty(void);
+bool is_full(void);
+void push(int input);
 int pop(void);
+void print_stack(void);
 void stack_underflow(void); 
 void stack_overflow(void);
 
@@ -15,7 +19,34 @@ int top = 0;
 int stack[STACK_SIZE];
 
 int main(void) {
-	// 	
+	int input;
+	char op;
+
+	PRINT_COMMANDS;
+	for (;;) {
+		printf("Enter command: ");
+		scanf(" %c", &op);
+		switch(op) {
+			case 'p':
+				printf("Enter an integer: ");
+				scanf(" %d", &input);
+				push(input);
+				break;
+			case 'o':
+				pop();
+				stack[top] = 0; 
+				break;
+			case 't':
+				print_stack(); break;
+			case 'e':
+				return 0;
+			default:
+				printf("Invalid command\n");
+				PRINT_COMMANDS;
+				break;
+		}
+	}
+	return 0;
 }
 
 void make_empty(void) {
@@ -30,11 +61,11 @@ bool is_full(void) {
 	return top == STACK_SIZE;
 }
 
-void push(int i) {
+void push(int input) {
 	if (is_full())
 		stack_overflow();
 	else
-		stack[top++] = c;
+		stack[top++] = input;
 }
 
 int pop(void) {
@@ -54,4 +85,11 @@ void stack_overflow(void) {
 void stack_underflow(void) {
 	printf("Stack underflow\n");
 	exit(1);
+}
+
+void print_stack(void) {
+	int i;
+
+	for (i = STACK_SIZE - 1; i >= 0; i--)
+		printf("%p %d\n", &stack[i], stack[i]);
 }
