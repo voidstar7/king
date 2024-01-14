7d942b62dd88dd27fa5b0d852b4c8b7b4ff8cb6c
ex07 <119447705+ex07@users.noreply.github.com>
2024-01-13 21:12:37 -0500

    use struct for stack item; implement push and print

--- 17-advanced-uses-of-pointers/stack-linked-list.c
+++ 17-advanced-uses-of-pointers/stack-linked-list.c
@@ -5,21 +5,23 @@
 #define STACK_SIZE 10
 #define PRINT_COMMANDS printf("Commands:\n(p) push\n(o) pop\n(t) print\n(e) exit\n")
 
+struct stackItem {
+	int value;
+	struct stackItem *next;
+};
 
 void make_empty(void);
 bool is_empty(void);
-bool is_full(void);
-void push(int input);
+struct stackItem *push(int input, struct stackItem *tail);
 int pop(void);
-void print_stack(void);
+void print_stack(struct stackItem *head); 
 void stack_underflow(void); 
-void stack_overflow(void);
 
-int top = 0;
-int stack[STACK_SIZE];
+int stackSize = 0;
 
 int main(void) {
-	int input;
+	struct stackItem *head, *tail;
+	int input; 
 	char op;
 
 	PRINT_COMMANDS;
@@ -30,14 +32,18 @@ int main(void) {
 			case 'p':
 				printf("Enter an integer: ");
 				scanf(" %d", &input);
-				push(input);
+				if (stackSize == 0)
+					head = tail = push(input, tail);
+				else
+					tail = push(input, tail);
 				break;
-			case 'o':
-				pop();
-				stack[top] = 0; 
-				break;
+		//	case 'o':
+		//		pop();
+		//		stack[top] = 0; 
+		//		break;
 			case 't':
-				print_stack(); break;
+				print_stack(head); 
+				break;
 			case 'e':
 				return 0;
 			default:
@@ -49,25 +55,35 @@ int main(void) {
 	return 0;
 }
 
-void make_empty(void) {
-	top = 0;
+//void make_empty(void) {
+//	top = 0;
+//}
+
+//bool is_empty(void) {
+//	return top == 0;
+//}
+
+struct stackItem *push(int input, struct stackItem *tail) {
+	struct stackItem *newItem;
+
+	newItem = malloc(sizeof(struct stackItem));
+	if (newItem == NULL) {
+		printf("Could not allocate memory for new stack item\n");
+		exit(1);
+	}
+	newItem->value = input;
+	if (stackSize == 0)
+		newItem->next = NULL;
+	else {
+		// newItem becomes the new tail
+		tail->next = newItem; 
+		newItem->next = NULL;
+	}
+	stackSize++;
+	return newItem;
 }
 
-bool is_empty(void) {
-	return top == 0;
-}
-
-bool is_full(void) {
-	return top == STACK_SIZE;
-}
-
-void push(int input) {
-	if (is_full())
-		stack_overflow();
-	else
-		stack[top++] = input;
-}
-
+/*
 int pop(void) {
 	if (is_empty()) {
 		stack_underflow();
@@ -76,20 +92,17 @@ int pop(void) {
 	else
 		return stack[--top];
 }
-
-void stack_overflow(void) {
-	printf("Stack overflow\n");
-	exit(1);
-}
+*/
 
 void stack_underflow(void) {
 	printf("Stack underflow\n");
 	exit(1);
 }
 
-void print_stack(void) {
-	int i;
+void print_stack(struct stackItem *head) {
+	struct stackItem *p;
 
-	for (i = STACK_SIZE - 1; i >= 0; i--)
-		printf("%p %d\n", &stack[i], stack[i]);
+	for (p = head; p->next != NULL; p = p->next)
+		printf("%p %d\n", p, p->value);
+	printf("%p %d\n", p, p->value);
 }
