8c06d9756546be8c19a19d4e33c8fd92db77300a
ex07 <119447705+ex07@users.noreply.github.com>
2024-01-25 22:18:18 -0500

    add words to string array

---
+++ 17-advanced-uses-of-pointers/word-sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_WORDS 3
+#define MAX_LEN 20
+
+int addWord(char *words[], int wordCount);
+
+int main(void) {
+	int wordCount = 0, i = 0, j, wordLength;
+	char *words[MAX_WORDS];
+
+	while (i++ < MAX_WORDS) {
+		if (addWord(words, wordCount++) == 0)
+			break;
+	}
+	for (j = 0; j < wordCount; j++)
+		printf("%s\n", words[j]);
+	return 0;
+}
+
+int addWord(char *words[], int wordCount) {
+  int input, i = 0;
+	char buffer[MAX_LEN];
+
+	printf("Enter word: ");
+  while (isspace(input = getchar()))
+    ;
+  while (input != '\n') {
+    if (i < MAX_LEN)
+      buffer[i++] = input;
+    input = getchar();
+  }
+  buffer[i] = '\0';
+	words[wordCount] = malloc(strlen(buffer) + 1);
+	if (words[wordCount] == NULL) {
+		printf("Error: unable to allocate memory\n");
+		exit(1);
+	}
+	strcpy(words[wordCount], buffer);
+	//free(words[wordCount]);
+  return i;
+}
