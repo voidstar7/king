b54f2139aa938b1d2e68fff06da8ebe0c63a9f34
ex07 <119447705+ex07@users.noreply.github.com>
2024-01-30 14:46:15 -0500

    update from array to ptr notation

--- 16-structures-unions-enums/inventory/inventory.c
+++ 16-structures-unions-enums/inventory/inventory.c
@@ -26,11 +26,11 @@ typedef struct {
 int sortOrder;
 int maxParts = 3;
 
-int find_part(Part inventory[], int number, int num_parts);
-int insert(Part inventory[], int num_parts);
-void search(Part inventory[], int num_parts);
-void update_quantity(Part inventory[], int num_parts);
-void update_price(Part inventory[], int num_parts);
+int find_part(Part *pi, int number, int num_parts);
+int insert(Part *pi, int num_parts);
+void search(Part *pi, int num_parts);
+void update_quantity(Part *pi, int num_parts);
+void update_price(Part *pi, int num_parts);
 void print(Part *pi, int num_parts);
 int compare_parts(const void *p, const void *q); 
 
@@ -67,13 +67,13 @@ int main(void)
     while (getchar() != '\n')   /* skips to end of line */
       ;
     switch (code) {
-      case 'i': num_parts += insert(inventory, num_parts);
+      case 'i': num_parts += insert(pi, num_parts);
                 break;
-      case 's': search(inventory, num_parts);
+      case 's': search(pi, num_parts);
                 break;
-      case 'u': update_quantity(inventory, num_parts);
+      case 'u': update_quantity(pi, num_parts);
                 break;
-      case '$': update_price(inventory, num_parts);
+      case '$': update_price(pi, num_parts);
                 break;
       case 'p': print(pi, num_parts);
                 break;
@@ -90,12 +90,12 @@ int main(void)
  *            array. Returns the array index if the part  *
  *            number is found; otherwise, returns -1.     *
  **********************************************************/
-int find_part(Part inventory[], int number, int num_parts)
+int find_part(Part *pi, int number, int num_parts)
 {
   int i;
 
   for (i = 0; i < num_parts; i++)
-    if (inventory[i].number == number)
+    if (pi[i].number == number)
       return i;
   return -1;
 }
@@ -107,25 +107,25 @@ int find_part(Part inventory[], int number, int num_parts)
  *         prematurely if the part already exists or the  *
  *         database is full.                              *
  **********************************************************/
-int insert(Part inventory[], int num_parts)
+int insert(Part *pi, int num_parts)
 {
   int part_number;
 
   if (num_parts == maxParts) {
-		Part *pi;
+		Part *newPi;
     printf("Database is full (%d parts). Allocating space for 3 more parts\n", num_parts);
-		pi = realloc(inventory, sizeof(Part) * maxParts); 
-		if (inventory == NULL) {
+		newPi = realloc(pi, sizeof(Part) * maxParts); 
+		if (newPi == NULL) {
 			printf("Error: failed to reallocate memory\n");
-			free(inventory);
+			free(pi);
 			exit(1);
 		}
-		if (pi == inventory)
-			printf("Reallocated memory (%p) has same address as initial block (%p)\n\n", pi, inventory);
+		if (pi == newPi)
+			printf("Reallocated memory (%p) has same address as initial block (%p)\n\n", newPi, pi);
 		else {
-			printf("Address of reallocated memory (%p) was moved by realloc(). Old location (%p)\n\n", pi, inventory);
-			inventory = pi;
-			printf("Updated inventory array pointer to point to %p\n", inventory);
+			printf("Address of reallocated memory (%p) was moved by realloc(). Old location (%p)\n\n", newPi, pi);
+			pi = newPi;
+			printf("Updated inventory array pointer to point to %p\n", newPi);
 			// this breaks the program when printing after updating the pointer
 		}
 		maxParts += 3;
@@ -133,18 +133,18 @@ int insert(Part inventory[], int num_parts)
   printf("Enter part number: ");
   scanf("%d", &part_number);
 	if (num_parts > 0) {
-		if (find_part(inventory, part_number, num_parts) >= 0) {
+		if (find_part(pi, part_number, num_parts) >= 0) {
 			printf("Part already exists.\n");
 			return 0;
 		}
   }
-  inventory[num_parts].number = part_number;
+  pi[num_parts].number = part_number;
   printf("Enter part name: ");
-  read_line(inventory[num_parts].name, NAME_LEN);
+  read_line(pi[num_parts].name, NAME_LEN);
   printf("Enter quantity on hand: ");
-  scanf("%d", &inventory[num_parts].on_hand);
+  scanf("%d", &pi[num_parts].on_hand);
   printf("Enter price: ");
-  scanf("%lf", &inventory[num_parts].price);
+  scanf("%lf", &pi[num_parts].price);
 	return 1;
 }
 
@@ -154,17 +154,17 @@ int insert(Part inventory[], int num_parts)
  *         exists, prints the name and quantity on hand;  *
  *         if not, prints an error message.               *
  **********************************************************/
-void search(Part inventory[], int num_parts)
+void search(Part *pi, int num_parts)
 {
   int i, number;
 
   printf("Enter part number: ");
   scanf("%d", &number);
-  i = find_part(inventory, number, num_parts);
+  i = find_part(pi, number, num_parts);
   if (i >= 0) {
-    printf("Part name: %s\n", inventory[i].name);
-    printf("Quantity on hand: %d\n", inventory[i].on_hand);
-    printf("Price: $%lf\n", inventory[i].price);
+    printf("Part name: %s\n", pi[i].name);
+    printf("Quantity on hand: %d\n", pi[i].on_hand);
+    printf("Price: $%lf\n", pi[i].price);
   } else
     printf("Part not found.\n");
 }
@@ -176,32 +176,32 @@ void search(Part inventory[], int num_parts)
  *         change in quantity on hand and updates the     *
  *         database.                                      *
  **********************************************************/
-void update_quantity(Part inventory[], int num_parts)
+void update_quantity(Part *pi, int num_parts)
 {
   int i, number, change;
 
   printf("Enter part number: ");
   scanf("%d", &number);
-  i = find_part(inventory, number, num_parts);
+  i = find_part(pi, number, num_parts);
   if (i >= 0) {
     printf("Enter change in quantity on hand: ");
     scanf("%d", &change);
-    inventory[i].on_hand += change;
+    pi[i].on_hand += change;
   } else
     printf("Part not found.\n");
 }
 
 // updates the price of a part
-void update_price(Part inventory[], int num_parts)
+void update_price(Part *pi, int num_parts)
 {
   int i, number;
 
   printf("Enter part number: ");
   scanf("%d", &number);
-  i = find_part(inventory, number, num_parts);
+  i = find_part(pi, number, num_parts);
   if (i >= 0) {
     printf("Enter new price: ");
-    scanf("%lf", &inventory[i].price);
+    scanf("%lf", &pi[i].price);
   } else
     printf("Part not found.\n");
 }
@@ -213,18 +213,18 @@ void update_price(Part inventory[], int num_parts)
  *        order in which they were entered into the       *
  *        database.                                       *
  **********************************************************/
-void print(Part *inventory, int num_parts)
+void print(Part *pi, int num_parts)
 {
 	Part *p;
 
 	printf("(1) sort ascending\n(2) sort descending: ");
 	scanf("%d", &sortOrder);
-	qsort(inventory, num_parts, sizeof(Part), compare_parts);
+	qsort(pi, num_parts, sizeof(Part), compare_parts);
   printf("Part Number   Part Name                Price         "
          "Quantity on Hand\n");
   for (
-			p = inventory;
-			p < inventory + num_parts; 
+			p = pi;
+			p < pi + num_parts; 
 			p++)
 				printf("%-14d%-25s$%-19.2lf%-11d %p\n", 
 						p->number, 
