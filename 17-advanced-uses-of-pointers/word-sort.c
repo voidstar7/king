f606a09d7b1a3c7478d238f5b7f42ca56b876697
ex07 <119447705+voidstar7@users.noreply.github.com>
2024-02-09 22:22:24 -0500

    store single string in array

--- 17-advanced-uses-of-pointers/word-sort.c
+++ 17-advanced-uses-of-pointers/word-sort.c
@@ -3,44 +3,23 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAX_WORDS 3
 #define MAX_LEN 20
 
-int addWord(char *words[], int wordCount);
-
 int main(void) {
-	int wordCount = 0, i = 0, j, wordLength;
-	char *words[MAX_WORDS];
-
-	while (i++ < MAX_WORDS) {
-		if (addWord(words, wordCount++) == 0)
-			break;
-	}
-	for (j = 0; j < wordCount; j++) {
-		printf("%s\n", words[j]);
-		free(words[j]);
-	}
-	return 0;
-}
-
-int addWord(char *words[], int wordCount) {
-  int input, i = 0;
-	char buffer[MAX_LEN];
+	int numWords = 0, len;
+	char *words[numWords], buffer[MAX_LEN + 1];
 
 	printf("Enter word: ");
-  while (isspace(input = getchar()))
-    ;
-  while (input != '\n') {
-    if (i < MAX_LEN && !(isspace(input)))
-      buffer[i++] = input;
-    input = getchar();
-  }
-  buffer[i] = '\0';
-	words[wordCount] = malloc(strlen(buffer) + 1);
-	if (words[wordCount] == NULL) {
-		printf("Error: unable to allocate memory\n");
-		exit(1);
+	fgets(buffer, MAX_LEN + 1, stdin);
+	len = strlen(buffer);
+	buffer[len] = '\0';
+	*words = malloc(len + 1);
+	if (*words == NULL) {
+		printf("Could not allocate memory\n");
+		return 1;
 	}
-	strcpy(words[wordCount], buffer);
-  return i;
+	strcpy(*words + (numWords++), buffer);
+
+	printf("%s\n", *words);
 }
+
