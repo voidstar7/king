ff6e901977888ca847462c54e0ec29ebe4d08d4c
ex07 <119447705+voidstar7@users.noreply.github.com>
2024-02-19 22:14:33 -0500

    create node from first word in stdin buffer

--- 17-advanced-uses-of-pointers/justify2/main.c
+++ 17-advanced-uses-of-pointers/justify2/main.c
@@ -5,15 +5,18 @@
 #define MAX_LINE_LEN 60
 #define MAX_WORD_LEN 16
 
-struct word {
+struct node {
 	char *letters;
 	int length;
 	struct word *next;
 };
 
+struct node *createNode(char *buffer);
+
 int main(void) {
-	char buffer[MAX_WORD_LEN], c;
 	int count = 0;
+	char buffer[MAX_WORD_LEN], c;
+	struct node *newNode;
 
 	while ((c = getchar()) == ' ' || c == '\t')
 		;
@@ -24,5 +27,28 @@ int main(void) {
 		c = getchar();
 	}
 	buffer[count++] = '\0';
+	// should i allocate newNode memory here and pass a pointer to newNode to createNode() probably should do it here right?
+	newNode = createNode(buffer);
+	printf("%s", newNode->letters);
+}
+
+struct node *createNode(char *buffer) {
+	struct node *word;
+	int wordLength = strlen(buffer);
+
+	// or do i allocate the memory here?
+	word = malloc(sizeof(struct node));
+	if (word == NULL) {
+		printf("Could not allocate memory for new node\n");
+		exit(1);
+	}
+	word->letters = malloc(wordLength);
+	if (word->letters == NULL) {
+		printf("Could not allocate memory for node string\n");
+		exit(1);
+	}
+	strcpy(word->letters, buffer);
+	word->length = wordLength;
+	return word;
 }
 
