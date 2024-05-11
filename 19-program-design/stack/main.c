83e22f424821bd8928e4595b6615c3d8ec711ea7
ex07 <119447705+voidstar7@users.noreply.github.com>
2024-05-11 06:19:05 -0400

    reset to start of exercise 4a
    implement a stack as a fixed length array; delete header and source files for stack2; update brace format; for 4b it should not be necessary to touch main.c only the module files

--- 19-program-design/stack/main.c
+++ 19-program-design/stack/main.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "stack.h"
+#include "stack1.h"
 
-Stack *s1, *s2;
+Stack *s1;
 
 int main(void) {
-	push(&s1, 72);
-	push(&s2, 22);
-	push(&s2, 288);
-	push(&s2, 888);
-	push(&s1, 88);
-	push(&s1, 92);
-	push(&s1, 18);
-	print_stack(&s1);
-	print_stack(&s2);
-	/*
-	make_empty(s1);
+	s1 = createStack(s1);
+	initialize(s1);
+	push(s1, 3);
 	push(s1, 8);
-	printf("pushed 8\n");
-	push(s1, 2);
-	printf("pushed 2\n");
-	printf("%d %d\n", 
-			s1->contents[0], 
-			s1->contents[1]);
-	printf("popped %d\n", pop(s1));
-	printf("popped %d\n", pop(s1));
-	pop(s1);
-	*/
+	push(s1, 5);
+	printf("Popped %i\n", pop(s1));
+	printf("Popped %i\n", pop(s1));
+	printf("Popped %i\n", pop(s1));
 }
--- 19-program-design/stack/stack1.c
+++ 19-program-design/stack/stack1.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "stack.h"
+#include "stack1.h"
 
-static void terminate(const char *message)
-{
+static void terminate(const char *message) {
   printf("%s\n", message);
   exit(EXIT_FAILURE);
 }
 
-void make_empty(Stack *s)
-{
+void initialize(Stack *s) {
   s->top = 0;
 }
 
-bool is_empty(Stack *s)
-{
+bool is_empty(Stack *s) {
   return s->top == 0;
 }
 
-bool is_full(Stack *s)
-{
-  return s->top == STACK_SIZE;
+bool is_full(Stack *s) {
+  return s->top == MAX_SIZE;
 }
 
-void push(Stack *s, int i)
-{
+void push(Stack *s, int i) {
   if (is_full(s))
-    terminate("Error in push: stack is full.");
-  s->contents[s->top] = i;
+    terminate("Error: stack is full");
+  s->data[s->top] = i;
 	s->top++;
+	printf("Pushed %i\n", i);
 }
 
-int pop(Stack *s)
-{
+int pop(Stack *s) {
 	int i;
   if (is_empty(s))
-    terminate("Error in pop: stack is empty.");
-	i = s->contents[(s->top) - 1];
+    terminate("Error: stack is empty");
+	i = s->data[(s->top) - 1];
 	s->top--;
-  return i;;
+  return i;
+}
+
+Stack *createStack(Stack *s) {
+	Stack *t = malloc(sizeof(Stack));
+	if (t == NULL) {
+		printf("Error: could not allocate memory for stack\n");
+		exit(1);
+	}
+	return t;
 }
